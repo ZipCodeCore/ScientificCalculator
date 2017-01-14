6f32c0cc8e6443cc23bbf5c177a4256e4f13395d
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-01-14 01:20:40 -0500

    Added unit testing for model

---
+++ src/test/java/squier/john/calculatorProject/CalculatorModelTest.java
@@ -0,0 +1,44 @@
+package squier.john.calculatorProject;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by johnsquier on 1/14/17.
+ */
+public class CalculatorModelTest {
+
+    CalculatorModel calculatorModel;
+    double delta;
+
+    @Before
+    public void setup() {
+        calculatorModel = new CalculatorModel();
+        delta = 0.00001;
+    }
+
+    @Test
+    public void getCurrentValueTest() {
+        double expected = 0.0;
+        double actual = calculatorModel.getCurrentValue();
+        Assert.assertEquals(expected, actual, delta);
+    }
+
+    @Test
+    public void setCurrentValueTest() {
+        double expected = 100.0;
+        calculatorModel.setCurrentValue(100.0);
+        double actual = calculatorModel.getCurrentValue();
+        Assert.assertEquals(expected, actual, delta);
+    }
+
+
+
+    @Test
+    public void getOperationsTest() {
+        Operations expected = new Operations();
+        Operations actual = calculatorModel.getOperations();
+        Assert.assertEquals(expected.getClass(), actual.getClass());
+    }
+}
