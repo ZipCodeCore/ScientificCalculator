a07a4c8063935c39447bd7ece963162bdc256505
marwamc <marwac@live.com>
2017-01-13 20:37:49 -0500

    "first commit, initialized maven project and created "DisplayManager" class."

---
+++ src/main/java/marwamilton/calculator/DisplayManager.java
@@ -0,0 +1,15 @@
+package marwamilton.calculator;
+
+/**
+ * Created by mkulima on 1/13/17.
+ */
+public class DisplayManager {
+    String displayedString;
+
+    public String getUserInput(){
+
+    }
+
+
+
+}
