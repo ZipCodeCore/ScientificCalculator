cab13a0b0e4bf8943a319854f2ba9ba9493e968f
William Mattern <williammattern@zipcoders-MacBook-Pro-3.local>
2017-01-16 18:32:35 -0500

    added UML and comments

--- src/main/java/mattern/william/CalculatorProject.java
+++ src/main/java/mattern/william/CalculatorProject.java
@@ -2,7 +2,11 @@ package mattern.william;
 
 /**
  * Created by williammattern on 1/13/17.
- */
+ * created in collaboration with Gerrod Mozeik.
+ * The main function calls the display class to initialize the display.
+ * The display class includes a switch statement that calls the Choice class early if it is a single variable function
+ * The Choice class calls the calculator functions.
+ *  */
 public class CalculatorProject {
 
     public static void main(String[] args) {
