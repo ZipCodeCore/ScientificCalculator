1a38eff7a2752137b12f4ec2a454482ad26b6e08
Gillian Reynolds-Titko <gillianreynolds-titko@zipcoders-MBP.matgamr.den>
2017-01-16 20:11:31 -0500

    Still not looping

--- src/main/java/reynoldstitko/gillian/Main.java
+++ src/main/java/reynoldstitko/gillian/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
 
-        controller.runCalculator();
+            controller.runCalculator();
     }
 }
