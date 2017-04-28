28f34265957f0fed47c271dad8068beb718bcd1a
Chris <chriskim80@gmail.com>
2017-04-28 19:40:26 -0400

    Added initial set of empty classes

---
+++ src/main/java/kim/christopher/App.java
@@ -0,0 +1,13 @@
+package kim.christopher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
