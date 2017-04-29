c99e60a841c31c8dac5764993f701565a6e18898
Chris <chriskim80@gmail.com>
2017-04-28 20:45:29 -0400

    Added testAddition stub to CalculatorTest.java

--- src/test/java/kim/christopher/CalculatorTest.java
+++ src/test/java/kim/christopher/CalculatorTest.java
@@ -6,4 +6,22 @@ import static junit.framework.Assert.*;
  * Created by christopherkim on 4/28/17.
  */
 public class CalculatorTest {
+
+    @Test
+    public void testAddition(){
+        //: Given
+        double firstOperand = 2, secondOperand = 2; // ... two positive values to add
+        double expectedSum = firstOperand + secondOperand; // 2 + 2 = 4
+        Controller controller = new Controller();
+
+
+        //: When
+        int actualSum = controller.calc.add(firstOperand, secondOperand);
+
+
+        //: Then
+        // ... The result should be the sum of those two values
+        assertEquals("2 + 2 should equal 4", expectedSum, actualSum);
+
+    }
 }
