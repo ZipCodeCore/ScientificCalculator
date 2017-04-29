eacb3fd86143b24308ae5250bdbc68f1d857077f
ant <anthonyjones@zipcoder3.home>
2017-04-28 23:37:17 -0400

    Doing Enum logic

--- src/main/java/com.anthony.calculator/Display.java
+++ src/main/java/com.anthony.calculator/Display.java
@@ -10,32 +10,43 @@ public class Display {
     String currentNumber;
     String givenNumber;
     Operation assignmentFromEnum;
+    int enumCount;
     Scanner in = new Scanner(System.in);
     double result;
+    boolean calculatorRunning=true;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        startingCalculatorDisplay();
 
     }
 
-    public void startingCalculatorDisplay(){
+    public static void startingCalculatorDisplay() {
+        System.out.println("Welcome to Anthony's simple yet, smart Calculator!");
+        System.out.println("Please enter an option of which operation you would like to perform");
+
         System.out.println("0");
     }
 
+    public void listOfEnumOperations(){
+        for(Operation enumLoop:assignmentFromEnum.values()){
+            System.out.println(enumLoop + ": "+ " ");
 
-    public void clearDisplay(){
+        }
+    }
 
+
+    public void clearDisplay() {
+    }
+
+    public void changeCurrentNumber(String number) {
 
     }
 
-    public void changeCurrentNumber(String number){
+    public void switchDisplayMode() {
 
     }
 
-    public void switchDisplayMode(){
-
-    }
-
-    public void switchDisplayMode(String mode){
+    public void switchDisplayMode(String mode) {
 
     }
 
