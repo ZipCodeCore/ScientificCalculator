5adf6f49128e45a371ecb9f4e0d5c7ddeebd7531
Chris <chriskim80@gmail.com>
2017-04-28 21:18:25 -0400

    Added InputReader field and no-args constructor for Calculator class

--- src/main/java/kim/christopher/Calculator.java
+++ src/main/java/kim/christopher/Calculator.java
@@ -4,4 +4,12 @@ package kim.christopher;
  * Created by christopherkim on 4/28/17.
  */
 public class Calculator {
+
+    InputReader reader;
+
+    public Calculator(){
+        reader = new InputReader();
+    }
+
+    
 }
