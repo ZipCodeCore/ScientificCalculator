f5848297af91f1162c97ae854bb36385c51bcb90
Byung-Chun Yoo <byung-chunyoo@zipcoder7.local>
2017-04-29 18:26:23 -0400

    Name changed

---
+++ src/main/java/yoo/Byung_chun/Calculator.java
@@ -0,0 +1,12 @@
+package yoo.Byung_chun;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator
+{
+    public static void main( String[] args ) {
+
+    }
+}
---
+++ src/test/java/yoo/Byung_chun/TestCalculator.java
@@ -0,0 +1,9 @@
+package yoo.Byung_chun;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class TestCalculator{
+
+}
