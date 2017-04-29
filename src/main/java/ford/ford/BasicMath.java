11eff5bfef14d99a88d9af97928592aa75497807
Rahmir Ford <Rahmir.ford@gmail.com>
2017-04-29 17:25:58 -0400

    Updating calculator

--- src/main/java/ford/ford/BasicMath.java
+++ src/main/java/ford/ford/BasicMath.java
@@ -1,38 +1,27 @@
 package ford.ford;
+import java.lang.*;
 
 /**
  * Created by rahmirford on 4/29/17.
  */
 public class BasicMath {
-    public double add(double x, double y){
-        return 0;
+
+    public double add(double x, double y){ return x + y;}
+
+    public float divide(float x, float y) {
+        if( y == 0) {
+            return NaN;
+        } else{
+            return x / y;
+        }
     }
 
-    public double divide(double x, double y) {
-        return 0;
-    }
+    public double subtract(double x, double y){return x - y;}
 
-    public double subtract(double x, double y){
-        return 0;
-    }
+    public double multiply(double x, double y) {return x * y;}
 
-    public double multiply(double x, double y) {
-        return 0;
-    }
+    public double findSquareRoot(double x) {return Math.sqrt(x);}
 
-    public double findSquareRoot(double x, double y) {
-        return 0;
-    }
-
-    public double findSquare(double x, double y){
-        return 0;
-    }
-
-
-
-
-
-
-
+    public double findSquare(double x){return x * x;}
 
 }
--- src/test/java/TestBasicMath.java
+++ src/test/java/TestBasicMath.java
@@ -25,6 +25,74 @@ public class TestBasicMath {
 
         //: Then
         Assert.assertEquals("The expected result is 7.0 ", expectedValue, actualValue, 0);
+
     }
+
+    @Test
+    public void testDivide() {
+        //: Given
+        double x = 4.0, y = 2.0;
+        double expectedValue = 2.0;
+
+        //: When
+        double actualValue = bm.divide(x, y);
+
+        //: Then
+        Assert.assertEquals("The expected value is 2.0", actualValue, expectedValue, 0);
+    }
+                     //Come back using try/catch statement in method
+//   @Test
+//   public void testDivideByZero(){
+//      double x = 4.0, y = 0;
+//      double expectedValue =4/0;
+//    }
+
+
+    @Test
+    public void testSubtract() {
+        //: Given
+        double x = 7.0, y = 2.0;
+        double expectedValue = 5.0;
+
+        //: When
+        double actualValue = bm.subtract(x, y);
+
+        //: Then
+        Assert.assertEquals("The expected value is 5.0", actualValue, expectedValue, 0);
+    }
+
+    @Test
+    public void testMultiply() {
+        double x = 7.0, y = 2.0;
+        double expectedValue = 14.0;
+
+        double actualValue = bm.multiply(x, y);
+
+        Assert.assertEquals("The expected value is 14.0", actualValue, expectedValue, 0);
+    }
+
+    @Test
+    public void testFindSquare() {
+
+        //: Given
+        double x = 3.0;
+        double expectedValue = 9.0;
+        //: When
+        double actualValue = bm.findSquare(x);
+        //: Then
+        Assert.assertEquals("The expected value of the square is 9.0", expectedValue, actualValue, 0);
+    }
+
+    @Test
+    public void testFindSquareRoot(){
+        //:Given
+        double x = 9;
+        double expectedValue = 3.0;
+        //: When
+        double actualValue = bm.findSquareRoot(x);
+        //: Then
+        Assert.assertEquals("The expected value of the square root is 3.0", expectedValue,actualValue,0);
+    }
+
 }
 
