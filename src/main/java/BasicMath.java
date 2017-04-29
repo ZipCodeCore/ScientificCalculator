22eedf101755d81a2095182a719408ef12b8bf3f
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-04-29 12:04:41 -0400

    Got basic operations working, basic tests working

---
+++ src/main/java/BasicMath.java
@@ -0,0 +1,33 @@
+
+public class BasicMath {
+
+    public double leftOperand;
+    public double rightOperand;
+    public double result;
+
+    public double add (double leftOperand, double rightOperand) {
+        result = leftOperand + rightOperand;
+        return result;
+    }
+
+    public double subtract (double leftOperand, double rightOperand) {
+        result = leftOperand - rightOperand;
+        return result;
+    }
+
+    public double divide (double leftOperand, double rightOperand) {
+        if (rightOperand == 0) {
+            System.out.println("Can not divide by zero");
+        } else {
+            result = leftOperand / rightOperand;
+        }
+        return result;
+    }
+
+
+    public double multiply (double leftOperand, double rightOperand) {
+        result = leftOperand * rightOperand;
+        return result;
+    }
+
+}
--- src/main/java/CalculatorApp.java
+++ src/main/java/CalculatorApp.java
@@ -1,12 +1,56 @@
+import java.util.Scanner;
+
 
 public class CalculatorApp {
 
-    public void CalcEngine () {}
+    public static void main (String [] args) {
+
+        double leftOperand;
+        double rightOperand;
+        double result = 0;
+        char operator;
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        leftOperand = userInput.nextDouble();
+        System.out.print("Choose an operator + , - , / , * : " );
+        operator = userInput.next().charAt(0);
+        System.out.print("Enter a number: ");
+        rightOperand = userInput.nextDouble();
+
+        BasicMath execute = new BasicMath();
+
+        switch (operator) {
+            case '+':
+                result = execute.add(leftOperand,rightOperand);
+                break;
+            case '-':
+                result = execute.subtract(leftOperand,rightOperand);
+                break;
+            case '/':
+                if (rightOperand != 0) {
+                    result = execute.divide(leftOperand,rightOperand);
+                } else {
+                    System.out.println("ERR - Divide by zero");
+                    break;
+                }
+                break;
+            case '*':
+                result = execute.multiply(leftOperand,rightOperand);
+                break;
+            default :
+                System.out.println("Invalid operation");
+
+        }
+
+        System.out.println("Result = " + result);
 
 
 
+
+
+    }
 }
-// Stub out methods
-// Create other classes
-// Stub out test methods
+
 
