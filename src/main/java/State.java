71a27a63763f9217184f75734f4f9b091a1dd37f
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.fios-router.home>
2017-04-29 00:19:28 -0400

    added state and statetest classes, mostly fleshed out

---
+++ src/main/java/State.java
@@ -0,0 +1,34 @@
+/**
+ * Created by danzygmund-felt on 4/28/17.
+ */
+public class State {
+    private double value;
+    private boolean hasError;
+
+    State() {
+        this(0);
+    }
+
+    State(double value) {
+        this.value = value;
+        this.hasError = false;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return this.value;
+    }
+
+    public void setError(boolean hasError) {
+        this.hasError = hasError;
+    }
+
+    public boolean isError() {
+        return this.hasError;
+    }
+
+
+}
