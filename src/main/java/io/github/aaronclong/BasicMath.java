116d09d9e9d50b0d117b7798ddc8252161af3fef
Aaron Long <aaron.long.c@gmail.com>
2017-04-29 12:43:14 -0400

    Finished BasicMath Class and Test

--- src/main/java/io/github/aaronclong/BasicMath.java
+++ src/main/java/io/github/aaronclong/BasicMath.java
@@ -4,8 +4,20 @@ package io.github.aaronclong;
  * Created by aaronlong on 4/29/17.
  */
 public class BasicMath {
+
+    public static double addition(double x, double y) {
+        return x + y;
+    }
+
+    public static double division(double x, double y) {
+        return x/y;
+    }
+
     public static double multiply(double x, double y) {
         return x * y;
     }
 
+    public static double subtract(double x, double y) {
+        return x - y;
+    }
 }
--- src/test/java/io/github/aaronclong/BasicMathTest.java
+++ src/test/java/io/github/aaronclong/BasicMathTest.java
@@ -7,6 +7,25 @@ import org.junit.Test;
  * Created by aaronlong on 4/29/17.
  */
 public class BasicMathTest {
+
+    @Test
+    public void testAddition() {
+        int seven = 7;
+        Assert.assertEquals("Confirming int addition", 14.0, BasicMath.addition(seven, seven),.003);
+        double fifteen = 15.0;
+        Assert.assertEquals("Confirming double addition", 30.0, BasicMath.addition(fifteen, fifteen),.003);
+    }
+
+    @Test
+    public void division() {
+        int twenty = 20;
+        int five = 4;
+        Assert.assertEquals("Confirm int division", 5.0, BasicMath.division(twenty, five),.003);
+        double fifteen = 15.0;
+        double three = 3.0;
+        Assert.assertEquals("Confirm double division", 5.0, BasicMath.division(fifteen, three),.003);
+    }
+
     @Test
     public void testMultiplication() {
         int four = 4;
@@ -16,7 +35,9 @@ public class BasicMathTest {
     }
 
     @Test
-    public void testAddition() {
-
+    public void testSubtraction() {
+        Assert.assertEquals("Confirming two ints can be subtracted", 3.0, BasicMath.subtract(4, 1), .003);
+        Assert.assertEquals("Confirming two doubles can be subtracted", 3.0, BasicMath.subtract(4.0, 1.0), .003);
     }
+
 }
