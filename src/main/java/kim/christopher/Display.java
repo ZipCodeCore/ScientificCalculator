99afc910cb882644bf0691d3ca666965ced99fec
Chris <chriskim80@gmail.com>
2017-04-28 21:48:21 -0400

    Added currentDisplay field, no-args constructor, and clearDisplay method to Display.java

--- src/main/java/kim/christopher/Display.java
+++ src/main/java/kim/christopher/Display.java
@@ -3,5 +3,13 @@ package kim.christopher;
 
 public class Display {
 
+    String currentDisplay;
 
+    public Display(){
+        currentDisplay = "0";
+    }
+
+    public void clearDisplay(){
+        currentDisplay = "0";
+    }
 }
