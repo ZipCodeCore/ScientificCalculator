cef7949db1713a611e723710263ca5f687106fc2
Chris <chriskim80@gmail.com>
2017-04-28 20:13:21 -0400

    Removed Input class and added Controller Class(stub)

---
+++ src/test/java/kim/christopher/ControllerTest.java
@@ -0,0 +1,9 @@
+package kim.christopher;
+
+import junit.framework.Test;
+import static junit.framework.Assert.*;
+/**
+ * Created by christopherkim on 4/28/17.
+ */
+public class ControllerTest {
+}
