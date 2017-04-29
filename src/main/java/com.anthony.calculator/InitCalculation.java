24e8c251c41389242050f4cbc866eafe55cc35da
ant <anthonyjones@zipcoder3.home>
2017-04-29 00:43:46 -0400

    Added more logic into Display, in the operationChoice method

---
+++ src/main/java/com.anthony.calculator/InitCalculation.java
@@ -0,0 +1,14 @@
+package com.anthony.calculator;
+
+/**
+ * Created by anthonyjones on 4/28/17.
+ */
+public class InitCalculation {
+
+
+    public static void main(String[] args) {
+       Display initCalc = new Display();
+
+
+    }
+}
