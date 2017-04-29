c19c292b08188a82a7e21bc72e271404c3e80dfe
Chris <chriskim80@gmail.com>
2017-04-29 11:56:46 -0400

    Revert "Removed references to InputReader class" testing revert This reverts commit 1c05dd10cbdd13c96441861f625dfdedfee068a4.

--- src/main/java/kim/christopher/Controller.java
+++ src/main/java/kim/christopher/Controller.java
@@ -5,12 +5,12 @@ public class Controller {
 
     Calculator calculator;
     Display display;
-
+    InputReader reader;
 
     public Controller(){
         calculator = new Calculator();
         display = new Display();
-
+        reader = new InputReader();
     }
 
     public void start(){
