0012a91a5d5991cbb750ac802e84466df85e7311
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.local>
2017-04-29 14:37:37 -0400

    added trig functions to ScientificCalculator + Test

--- src/main/java/ScientificCalculator.java
+++ src/main/java/ScientificCalculator.java
@@ -3,6 +3,26 @@
  */
 public class ScientificCalculator extends Calculator {
     public double sin(double x) {
-        
+        return Math.sin(x);
+    }
+
+    public double cos(double x) {
+        return Math.cos(x);
+    }
+
+    public double tan(double x) {
+        return Math.tan(x);
+    }
+
+    public double asin(double x) {
+        return Math.asin(x);
+    }
+
+    public double acos(double x) {
+        return Math.acos(x);
+    }
+
+    public double atan(double x) {
+        return Math.atan(x);
     }
 }
--- src/test/java/ScientificCalculatorTest.java
+++ src/test/java/ScientificCalculatorTest.java
@@ -8,7 +8,7 @@ public class ScientificCalculatorTest {
     @Test
     public void sinTest() {
         //Given
-        Calculator calc = new Calculator();
+        ScientificCalculator calc = new ScientificCalculator();
         double expected = 1.0;
         //When
         double actual = calc.sin(Math.PI / 2);
@@ -19,7 +19,7 @@ public class ScientificCalculatorTest {
     @Test
     public void cosTest() {
         //Given
-        Calculator calc = new Calculator();
+        ScientificCalculator calc = new ScientificCalculator();
         double expected = 0.0;
         //When
         double actual = calc.cos(Math.PI / 2);
@@ -30,7 +30,7 @@ public class ScientificCalculatorTest {
     @Test
     public void tanTest() {
         //Given
-        Calculator calc = new Calculator();
+        ScientificCalculator calc = new ScientificCalculator();
         double expected = 1.0;
         //When
         double actual = calc.tan(Math.PI / 4);
@@ -38,5 +38,47 @@ public class ScientificCalculatorTest {
         Assert.assertEquals("tan(pi/4) = 1", expected, actual, 0);
     }
 
+    @Test
+    public void tanCosineEqualsZeroTest() {
+        //Given
+        ScientificCalculator calc = new ScientificCalculator();
+        //When
+        double result = calc.tan(Math.PI / 2);
+        //Then
+        Assert.assertTrue(Double.isNaN(result));
+    }
+
+    @Test
+    public void asinTest() {
+        //Given
+        ScientificCalculator calc = new ScientificCalculator();
+        double expected = Math.PI / 3;
+        //When
+        double actual = calc.asin(Math.sqrt(3.0) / 2);
+        //Then
+        Assert.assertEquals("arcsin of root three over two is pi/3", expected, actual, 0);
+    }
+
+    @Test
+    public void acosTest() {
+        //Given
+        ScientificCalculator calc = new ScientificCalculator();
+        double expected = Math.PI / 3;
+        //When
+        double actual = calc.acos(0.5);
+        //Then
+        Assert.assertEquals("arccos(0.5) = pi/3", expected, actual, 0);
+    }
+
+    @Test
+    public void atanTest() {
+        //Given
+        ScientificCalculator calc = new ScientificCalculator();
+        double expected = 0.0;
+        //When
+        double actual = calc.atan(0.0);
+        //Then
+        Assert.assertEquals("arctan(0) = 0", expected, actual, 0);
+    }
 
 }
