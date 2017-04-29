6843bdac18db42c4d9f9229e3cbb9e416edc3394
ant <anthonyjones@zipcoder3.home>
2017-04-28 22:04:03 -0400

    Edited test classes with add,subtract,multiply and divide

--- src/main/java/BasicMath.java
+++ src/main/java/BasicMath.java
@@ -2,4 +2,28 @@
  * Created by anthonyjones on 4/28/17.
  */
 public class BasicMath {
+
+
+    public double add(double number1, double number2) {
+        return number1 + number2;
+    }
+
+    public double subtract(double number1, double number2) {
+        return number1 - number2;
+    }
+
+    public double multply(double number1, double number2) {
+        return number1 * number2;
+    }
+
+    public double divide(double number1, double number2) {
+
+        if (number2 == 0) {
+            return Double.NaN;
+        }
+
+        return number1 / number2;
+    }
+
+
 }
--- src/test/java/testBasicMath.java
+++ src/test/java/testBasicMath.java
@@ -1,5 +1,86 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by anthonyjones on 4/28/17.
  */
 public class testBasicMath {
+
+    @Test
+    public void testAdd() {
+        //given
+        double number1 = 4;
+        double number2 = 5;
+        double expectedSum = number1 + number2;
+        BasicMath basicmather = new BasicMath();
+
+        //when
+        double actualSum = basicmather.add(number1, number2);
+
+        //then
+        Assert.assertEquals("Are you correct", expectedSum, actualSum, 0);
+
+    }
+
+    @Test
+    public void testSubtract() {
+        //given
+        double number1 = 4;
+        double number2 = 5;
+        double expectedSum = number1 - number2;
+        BasicMath basicmather = new BasicMath();
+
+        //when
+        double actualSum = basicmather.subtract(number1, number2);
+
+        //then
+        Assert.assertEquals("Are you correct", expectedSum, actualSum, 0);
+    }
+
+    @Test
+    public void testMultiply() {
+        //given
+        double number1 = 4;
+        double number2 = 5;
+        double expectedSum = number1 * number2;
+        BasicMath basicmather = new BasicMath();
+
+        //when
+        double actualSum = basicmather.multply(number1, number2);
+
+        //then
+        Assert.assertEquals("Are you correct", expectedSum, actualSum, 0);
+    }
+
+    @Test
+    public void testDivide() {
+        //given
+        double number1 = 4;
+        double number2 = 5;
+        double expectedSum = number1 / number2;
+        BasicMath basicmather = new BasicMath();
+
+        //when
+        double actualSum = basicmather.divide(number1, number2);
+
+        //then
+        Assert.assertEquals("Are you correct", expectedSum, actualSum, 1);
+    }
+
+    @Test
+    public void testDivideByZero() {
+        //given
+        double number1 = 4;
+        double number2 = 0;
+        double expectedSum = number1 / number2;
+        BasicMath basicmather = new BasicMath();
+
+        //when
+        double actualSum = basicmather.divide(number1, number2);
+
+        //then
+        Assert.assertTrue(Double.isNaN(actualSum));
+    }
+
+
 }
