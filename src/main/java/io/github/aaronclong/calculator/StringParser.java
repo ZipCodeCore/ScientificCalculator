5f5847c7315469a7b9afd0404f9b30977db9707a
Aaron Long <aaron.long.c@gmail.com>
2017-04-30 17:28:03 -0400

    Cleaned up parse method

--- src/main/java/io/github/aaronclong/calculator/StringParser.java
+++ src/main/java/io/github/aaronclong/calculator/StringParser.java
@@ -29,28 +29,43 @@ public class StringParser {
     }
 
     private void parse() {
-        double previous = 0.0;
-        double currentValue = 0.0;
-        String basicOperation = null;
-        for (String instruction : lexed) {
-            String[] details = instruction.split(" ");
-            if (details[0].equals(Lexer.WHITESPACE.name()))  { continue; }
-            else if (details[0].equals(Lexer.NUMBER.name())) {
-                if (basicOperation != null) {
-                    currentValue = Double.parseDouble(details[1]);
-                    currentValue = BasicMath.findOperation(basicOperation, previous, currentValue);
-                    basicOperation = null;
-                    continue;
+        parsed = new ParseObject().getValue();
+    }
+
+    private class ParseObject {
+        private double previous;
+        private double currentValue;
+        private String basicOperation;
+
+        public ParseObject() {
+            for (String instruction : lexed) {
+                String[] details = instruction.split(" ");
+                if (details[0].equals(Lexer.WHITESPACE.name()))  { continue; }
+                else if (details[0].equals(Lexer.NUMBER.name())) {
+                    handleNumbers(details[1]);
                 }
-                previous = currentValue;
-                currentValue = Double.parseDouble(details[1]);
+                else if (details[0].equals(Lexer.BASICOP.name())) {
+                    basicOperation = details[1];
+                    previous = currentValue;
+                }
             }
-            else if (details[0].equals(Lexer.BASICOP.name())) {
-                basicOperation = details[1];
-                previous = currentValue;
-            }
+            parsed = currentValue;
         }
-        parsed = currentValue;
+
+        public double getValue() {
+            return currentValue;
+        }
+
+        private void handleNumbers(String nums) {
+            if (basicOperation != null) {
+                currentValue = Double.parseDouble(nums);
+                currentValue = BasicMath.findOperation(basicOperation, previous, currentValue);
+                basicOperation = null;
+                return;
+            }
+            previous = currentValue;
+            currentValue = Double.parseDouble(nums);
+        }
     }
 
     private void iterator() {
