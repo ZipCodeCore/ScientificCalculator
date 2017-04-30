04b6589056a0ef0968c3e00d0acd82f5eeb6dd3e
Chris <chriskim80@gmail.com>
2017-04-29 20:22:59 -0400

    Moved some control logic from start() to readInput()

--- src/main/java/kim/christopher/Controller.java
+++ src/main/java/kim/christopher/Controller.java
@@ -23,34 +23,33 @@ public class Controller {
         display.setDisplay(reader.readLine().get(0));
 
         while(!quit) {
-//            System.out.println("Enter operator and 2nd operand: ");
-
-            ArrayList<String> test = reader.readLine();
-            if(test.get(0).equals("quit")) {
-                quit = true;
-                System.out.println("Goodbye!");
-            } else if (test.get(0).equals("clear")) {
-                display.clearDisplay();
-
-            } else if (test.get(0).equals("+")) {
-                display.setDisplay(calc.add(display.getDisplay(), test.get(1)));
-            } else if (test.get(0).equals("-")) {
-                display.setDisplay(calc.subtract(display.getDisplay(), test.get(1)));
-            } else if (test.get(0).equals("*")) {
-                display.setDisplay(calc.multiply(display.getDisplay(), test.get(1)));
-            } else if (test.get(0).equals("/")) {
-                if(calc.divide(display.getDisplay(), test.get(1)).equals("ERROR")){
-                    System.out.println("ERROR");
-                }
-                else display.setDisplay(calc.divide(display.getDisplay(), test.get(1)));
-            } else if(test.get(0).equals("negate")){
-                display.setDisplay(calc.negate(display.getDisplay()));
-            } else System.out.println("Invalid Operator");
+           readInput(quit);
         }
+    }
 
+    void readInput(boolean quit){
 
+        ArrayList<String> input = reader.readLine();
 
-
+        if(input.get(0).equals("quit")) {
+            quit = true;
+            System.out.println("Goodbye!");
+        } else if (input.get(0).equals("clear")) {
+            display.clearDisplay();
+        } else if (input.get(0).equals("+")) {
+            display.setDisplay(calc.add(display.getDisplay(), input.get(1)));
+        } else if (input.get(0).equals("-")) {
+            display.setDisplay(calc.subtract(display.getDisplay(), input.get(1)));
+        } else if (input.get(0).equals("*")) {
+            display.setDisplay(calc.multiply(display.getDisplay(), input.get(1)));
+        } else if (input.get(0).equals("/")) {
+            if(calc.divide(display.getDisplay(), input.get(1)).equals("ERROR")){
+                System.out.println("ERROR");
+            }
+            else display.setDisplay(calc.divide(display.getDisplay(), input.get(1)));
+        } else if(input.get(0).equals("negate")){
+            display.setDisplay(calc.negate(display.getDisplay()));
+        } else System.out.println("Invalid Operator");
     }
 
 }
