70aad144bd6b1c0733548aff17a70bec7a6bbba2
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.fios-router.home>
2017-04-29 20:03:44 -0400

    started on SpecialFunctionCalculator class

---
+++ src/main/java/SpecialFunctionCalculator.java
@@ -0,0 +1,6 @@
+/**
+ * Created by danzygmund-felt on 4/29/17.
+ */
+public class SpecialFunctionCalculator extends ScientificCalculator {
+
+}
---
+++ src/test/java/SpecialFunctionCalculatorTest.java
@@ -0,0 +1,24 @@
+/**
+ * Created by danzygmund-felt on 4/29/17.
+ */
+import org.junit.*;
+
+public class SpecialFunctionCalculatorTest {
+
+    @Test
+    public void isPrimeNotTest() {
+        //Given
+    }
+
+    @Test
+    public void isPrimeYesTest() {
+        //Given
+    }
+
+    @Test
+    public void isPrimeNonIntegerTest() {
+        //Given
+    }
+
+
+}
