389605f7d1ae37564fc62f40aab411ef5e0dd53e
Rahmir Ford <Rahmir.ford@gmail.com>
2017-04-29 20:12:51 -0400

    Pushed it real good

--- src/main/java/ford/ford/CalculatorInput.java
+++ src/main/java/ford/ford/CalculatorInput.java
@@ -1,10 +1,18 @@
 package ford.ford;
 
+import java.util.Scanner;
+
 /**
  * Created by rahmirford on 4/29/17.
  */
 public class CalculatorInput {
-    public double userInput(double x){ return 0;}
+    public double userInput() {
+        Scanner sc = new Scanner(System.in);
+        double input;
+        input = sc.nextDouble();
+        return input;
+    }
+
 
     public char userOperator(char x){
         return 0;
