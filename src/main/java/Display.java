03041a70593e9842b40ad5e29943df823a77b744
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.local>
2017-04-30 12:18:14 -0400

    Very ugly, single-reponsibility-violating main method to handle logic of taking input. Plus my edited Display class no longer has single responsibility.

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -4,18 +4,18 @@
 
 public class Display {
 
-    private DisplayMode displayMode;
+    DisplayMode displayMode;
+    private double display;
 
     Display() {
         this.setDisplay(0.0);
-        DisplayMode displayMode = new DisplayMode();
-    }
-
-    public void setDisplay() {
-        System.out.println("Err");
     }
 
     public void setDisplay(double x) {
+        if(Double.isNaN(x)) {
+            System.out.println("Err");
+        }
+        this.display = x;
         System.out.println(displayMode.toDisplayMode(x));
     }
 
@@ -23,7 +23,8 @@ public class Display {
         this.setDisplay(0.0);
     }
 
-    //not worrying about a getDisplay method right now, since it seems a little redundant and will make my life difficult
-    //but if I have to...
+    public double getDisplay() {
+        return this.display;
+    }
 
 }
---
+++ src/main/java/InputReader.java
@@ -0,0 +1,117 @@
+/**
+ * Created by danzygmund-felt on 4/30/17.
+ */
+import java.util.*;
+
+public class InputReader {
+
+    //from http://stackoverflow.com/questions/1102891/how-to-check-if-a-string-is-numeric-in-java
+    private static boolean isNumeric(String str) {
+        try {
+            double d = Double.parseDouble(str);
+        }
+        catch(NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        ScientificCalculator calc = new ScientificCalculator();
+        Display display = new Display();
+        OperatorChecker checker = new OperatorChecker();
+        Scanner scan = new Scanner(System.in);
+        String input;
+        double nextOperand;
+
+        while (true) {
+            input = scan.nextLine();
+            if (checker.isUnaryOperation(input)) {
+                switch (input) {
+                    case "sq":
+                        display.setDisplay(calc.square(display.getDisplay()));
+                        break;
+                    case "sqrt":
+                        display.setDisplay(calc.squareRoot(display.getDisplay()));
+                        break;
+                    case "inv":
+                        display.setDisplay(calc.inverse(display.getDisplay()));
+                        break;
+                    case "opp":
+                        display.setDisplay(calc.opposite(display.getDisplay()));
+                        break;
+                    case "sin":
+                        display.setDisplay(calc.sin(display.getDisplay()));
+                        break;
+                    case "cos":
+                        display.setDisplay(calc.cos(display.getDisplay()));
+                        break;
+                    case "tan":
+                        display.setDisplay(calc.tan(display.getDisplay()));
+                        break;
+                    case "asin":
+                        display.setDisplay(calc.asin(display.getDisplay()));
+                        break;
+                    case "acos":
+                        display.setDisplay(calc.acos(display.getDisplay()));
+                        break;
+                    case "atan":
+                        display.setDisplay(calc.atan(display.getDisplay()));
+                        break;
+                    case "log10":
+                        display.setDisplay(calc.log10(display.getDisplay()));
+                        break;
+                    case "tenpower":
+                        display.setDisplay(calc.tenPower(display.getDisplay()));
+                        break;
+                    case "log":
+                        display.setDisplay(calc.log(display.getDisplay()));
+                        break;
+                    case "epower":
+                        display.setDisplay(calc.ePower(display.getDisplay()));
+                        break;
+                    case "factorial":
+                        display.setDisplay(calc.factorial(display.getDisplay()));
+                        break;
+                    default:
+                        display.setDisplay(Double.NaN);
+                        break;
+                }
+            } else if (checker.isBinaryOperation(input)) {
+                nextOperand = scan.nextDouble();
+                switch (input) {
+                    case "+":
+                        display.setDisplay(calc.add(display.getDisplay(), nextOperand));
+                        break;
+                    case "-":
+                        display.setDisplay(calc.subtract(display.getDisplay(), nextOperand));
+                        break;
+                    case "*":
+                        display.setDisplay(calc.multiply(display.getDisplay(), nextOperand));
+                        break;
+                    case "/":
+                        display.setDisplay(calc.divide(display.getDisplay(), nextOperand));
+                        break;
+                    default:
+                        display.setDisplay(Double.NaN);
+                        break;
+                }
+            } else if (isNumeric(input)) {
+                display.setDisplay(Double.parseDouble(input));
+            } else if (input.equals("quit")) {
+                break;
+            } else if (input.equals("clear")) {
+                display.clearDisplay();
+            } else {
+                display.setDisplay(Double.NaN);
+                do {
+                    input = scan.nextLine();
+                    if (isNumeric(input)) {
+                        display.setDisplay(Double.parseDouble(input));
+                    }
+                } while (!isNumeric(input));
+            }
+        }
+    }
+}
