90d95c8aaf50223eeddff6b9725ca13560546361
Dandridge Cole <dandridgecole@yahoo.com>
2019-05-24 18:11:29 -0400

    Extended Feature Method Stubs
    Getting the first draft of the method stubs up to Joanna can start creating test cases

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Extended.java
@@ -0,0 +1,77 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class Extended {
+
+    public static void main(String[] args) {
+
+    }
+    public void switchDisplayMode(){
+
+    }
+
+    public void switchDisplayMode(String mode){
+
+    }
+
+    public void memClear(){
+
+    }
+
+    public void memPlus(){
+
+    }
+
+    public double memRecal(){
+        return 0;
+    }
+
+    public double cosine(double input){
+        return 0;
+    }
+
+    public double tangent(double input){
+        return 0;
+    }
+
+    public double invSine(double input){
+        return 0;
+    }
+
+    public double invCosine(double input){
+        return 0;
+    }
+
+    public double invTan(double input){
+        return 0;
+    }
+
+    public void switchUnitsMode(){
+        
+    }
+
+    public void switchUnitsMode(String mode){
+
+    }
+
+    public double log(double input){
+        return 0;
+    }
+
+    public double invLog(double input){
+        return 0;
+    }
+
+    public double ln(double input){
+        return 0;
+    }
+
+    public double invLn(double input){
+        return 0;
+    }
+
+    public double factorial(double input){
+        return 0;
+    }
+
+
+}
