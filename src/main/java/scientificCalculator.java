a9bb2e9e13cce1976ead45b50668fa919b27018a
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-05-24 18:21:22 -0400

    stubbed files

---
+++ src/main/java/scientificCalculator.java
@@ -0,0 +1,59 @@
+public class scientificCalculator {
+    public String state;
+    public Double currentNumber;
+
+    public scientificCalculator(Double x, Double y) {
+
+    }
+    public scientificCalculator(Double x) {}
+
+    public Double switchDisplayMode(String mode, Double value) {
+        return null;
+    } //b, o, h
+    public Double switchDisplayMode() {
+        return null;
+    } //toggle through binary, octal, hex
+    public Double saveNumber(String memStatus) {
+        return null;
+    } //save up to one number for later
+
+     public Double sine(Double x){
+        return null;
+     }
+     public Double cosine(Double x){
+        return null;
+     }
+     public Double tangent(Double x){
+        return null;
+     }
+     public Double arcSine(Double x){
+        return null;
+     }
+     public Double arcCos(Double x){
+        return null;
+     }
+     public Double arcTan(Double x){
+        return null;
+     }
+     public Double switchUnitsMode()  {
+        return null;
+     }
+     public Double switchUnitesMode(String mode) {
+        return null;
+     }
+     public Double log(Double x, Double y) {
+        return null;
+     }
+     public Double invLog(Double x, Double y) {
+        return null;
+     }
+     public Double ln(Double x, Double y) {
+        return null;
+     }
+     public Double invLn(Double x, Double y) {
+        return null;
+     }
+     public Double factorial(Double x) {
+        return null;
+     }
+}
