d36e7deacdec63a98dbea0625d9df3da225e9420
Robert Rieger <robertrieger@zipcoders-MacBook-Pro-96.local>
2019-05-24 18:31:09 -0400

    test

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -13,5 +13,7 @@ public class MainApplication {
         Console.println("The user input %s as a string", s);
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a d", d);
+
+
     }
 }
