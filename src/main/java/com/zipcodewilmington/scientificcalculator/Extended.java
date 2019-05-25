fe3d7431c5f6a993d0ba38c91417e2144a8652d3
Dandridge Cole <dandridgecole@yahoo.com>
2019-05-25 15:11:32 -0400

    Adding Sine function, commenting out less preferred

--- src/main/java/com/zipcodewilmington/scientificcalculator/Extended.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Extended.java
@@ -1,4 +1,5 @@
 package com.zipcodewilmington.scientificcalculator;
+import java.util.Arrays;
 /*
 enum trigUnits {
     RADIANS, DEGREES;
@@ -12,38 +13,41 @@ public class Extended {
 
     public trigUnits currentTrigUnits = trigUnits.DEGREES;
     */
-    private Character[] displayModes = {'b', 'o', 'd', 'h'};
+//    private Character[] displayModesChar = {'b', 'o', 'd', 'h'};
+    private String[] displayModeNames = {"binary","octal","decimal","hexadecimal"};
 
-    private Character[] trigUnits = {'r','d'};
+//    private Character[] trigUnitsChar = {'r','d'};
+    private String[] trigUnitNames = {"radians","degrees"};
 
-    public Character currentTrigUnits = trigUnits[0];
+//    public Character currentTrigUnits = trigUnitsChar[0];
+    private Integer curTrigUnitsIndex=0;
 
-    public Character currentDisplayMode = displayModes[2];
+//    public Character currentDisplayMode = displayModesChar[2];
+    private Integer curDisplayModeIndex=2;
 
-    public double memory=0;
-
+    private double memory=0;
 
     public static void main(String[] args) {
 
     }
 
     public String convertOutput (double output){
-        String converted="";
-        switch (currentDisplayMode) {
-            case 'b': {
+        String converted;
+        switch (curDisplayModeIndex) {
+            case 0: {
                 converted = Long.toBinaryString(Double.doubleToRawLongBits(output));
                 break;
             }
-            case 'o': {
+            case 1: {
                 converted = Long.toOctalString(Double.doubleToRawLongBits(output));
                 break;
             }
-            case 'h': {
+            case 3: {
                 converted = Double.toHexString(output);
                 break;
             }
             default: {
-                converted = String.format("%d", output);
+                converted = String.format("%1.10f", output);
                 break;
             }
         }
@@ -51,31 +55,33 @@ public class Extended {
     }
 
     public void switchDisplayMode() {
-        switch (currentDisplayMode) {
-            case 'b': {
-                currentDisplayMode=displayModes[1];
-                break;
-            }
-            case 'd': {
-                currentDisplayMode = displayModes[3];
-                break;
-            }
-            case 'h': {
-                currentDisplayMode = displayModes[0];
-                break;
-            }
-            default: {
-                currentDisplayMode = displayModes[2];
-                break;
-            }
-        }
+        if (curDisplayModeIndex > 2) curDisplayModeIndex=0;
+        else curDisplayModeIndex++;
     }
 
     public void switchDisplayMode(String mode){
-        if(mode.equals("binary")) currentDisplayMode = displayModes[0];
-        else if(mode.equals("octal")) currentDisplayMode = displayModes[1];
-        else if(mode.equals("hexadecimal")) currentDisplayMode = displayModes[3];
-        else currentDisplayMode = displayModes[2];
+        if(mode.equals(displayModeNames[0])) curDisplayModeIndex=0;
+        else if(mode.equals(displayModeNames[1])) curDisplayModeIndex=1;
+        else if(mode.equals(displayModeNames[2])) curDisplayModeIndex=2;
+        else if(mode.equals(displayModeNames[3])) curDisplayModeIndex=3;
+    }
+
+    public String getCurDisplayModeName(){
+        return displayModeNames[curDisplayModeIndex];
+    }
+
+    public String[] getDisplayModeOptions(){
+     //   return Arrays.copyOf(displayModeNames,displayModeNames.length);
+        String[] returnArray=new String[displayModeNames.length];
+        System.arraycopy(displayModeNames,0,returnArray,0,displayModeNames.length);
+        return returnArray;
+    }
+
+    public String[] getTrigModeOptions(){
+        //   return Arrays.copyOf(displayModeNames,displayModeNames.length);
+        String[] returnArray=new String[trigUnitNames.length];
+        System.arraycopy(trigUnitNames,0,returnArray,0,trigUnitNames.length);
+        return returnArray;
     }
 
     public void memClear(){
@@ -90,43 +96,52 @@ public class Extended {
         return memory;
     }
 /*
-    public double convertDegreesToRadians (double degrees){
+    private double convertDegreesToRadians (double degrees){
         return degrees*Math.PI/180;
     }
     */
+    public double sine(double input){
+        if(curTrigUnitsIndex==1) input = Math.toRadians(input);
+        return Math.sin(input);
+    }
+
     public double cosine(double input){
-        if(currentTrigUnits=='d') input = Math.toRadians(input);
+        if(curTrigUnitsIndex==1) input = Math.toRadians(input);
         return Math.cos(input);
     }
 
     public double tangent(double input){
-        if(currentTrigUnits=='d') input = Math.toRadians(input);
+        if(curTrigUnitsIndex==1) input = Math.toRadians(input);
         return Math.tan(input);
     }
 
     public double invSine(double input){
-        if(currentTrigUnits=='d') input = Math.toRadians(input);
+        if(curTrigUnitsIndex==1) input = Math.toRadians(input);
         return Math.asin(input);
     }
 
     public double invCosine(double input){
-        if(currentTrigUnits=='d') input = Math.toRadians(input);
+        if(curTrigUnitsIndex==1) input = Math.toRadians(input);
         return Math.acos(input);
     }
 
     public double invTan(double input){
-        if(currentTrigUnits=='d') input = Math.toRadians(input);
+        if(curTrigUnitsIndex==1) input = Math.toRadians(input);
         return Math.atan(input);
     }
 
     public void switchUnitsMode(){
-        if(currentTrigUnits==trigUnits[0])currentTrigUnits=trigUnits[1];
-        else currentTrigUnits = trigUnits[0];
+        if(curTrigUnitsIndex==0)curTrigUnitsIndex++;
+        else curTrigUnitsIndex=0;
     }
 
     public void switchUnitsMode(String mode){
-        if(mode.equals("degrees")) currentTrigUnits=trigUnits[1];
-        else currentTrigUnits = trigUnits[0];
+        if(mode.equals(trigUnitNames[0])) curTrigUnitsIndex=0;
+        else if(mode.equals(trigUnitNames[1]))curTrigUnitsIndex = 1;
+    }
+
+    public String getCurTrigUnitsName(){
+        return trigUnitNames[curTrigUnitsIndex];
     }
 
     public double log(double input){
@@ -148,13 +163,14 @@ public class Extended {
     public double factorial(double input){
         double myResult=1;
         if(input%1==0){
-            for(int i=Math.abs(input);i>1){
+            for(Integer i=(int)Math.abs(input);i>1;i++){
                 myResult *= i;
             }
             if(input<0) myResult *= (-1);
             return myResult;
         }
         return 0;
+
     }
 
 
