c98aa8989c0bb911ee110518cc4e819ce5b340bf
Robert Rieger <robertrieger@zipcoders-MacBook-Pro-96.local>
2019-05-27 00:47:20 -0400

    merged with calculator and calculator test

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -1,52 +1,52 @@
 package com.zipcodewilmington.scientificcalculator;
+import java.lang.Math;
 
 public class Calculator {
 
     public Calculator() {
-
     }
 
-
-
-
-    public  Double add (Double a, Double b){
-
+    public static Double add (Double a, Double b){
         return a + b;
     }
 
-    public  Double divide (Double a, Double b){
-
-        return a/b;
-    };
-
     public Double subtract (Double a, Double b){
-        return null;
+        return a - b;
     }
 
     public Double multiply(Double a, Double b){
-        return null;
+        return a * b;
+    }
+    public Double divide(Double a, Double b) {
+        return a / b;
     }
 
     public Double sqrt(Double a){
-        return null;
+        return Math.sqrt(a);
     }
 
     public Double sq(Double a){
-        return null;
+        return Math.pow(a, (double)2);
     }
 
     public Double exp(Double a, Double b){
-        return null;
+        return Math.pow(a, b);
     }
 
     //inverse
     public Double inv(Double a){
-        return null;
+        return 1 / a;
     }
 
     //if positive return neg / if neg return pos
     public Double negate(Double a){
-        return null;
+        Double answer = 0.0;
+        if(a > 0.0)
+            answer = a * -1;
+        else if(a < 0.0)
+            answer = Math.abs(a);
+
+        return answer;
     }
 
 }
--- src/test/java/com/zipcodewilmington/scientificcalculator/CalculatorTest.java
+++ src/test/java/com/zipcodewilmington/scientificcalculator/CalculatorTest.java
@@ -8,34 +8,268 @@ public class CalculatorTest {
 
     @Test
     public void add() {
+        Double a = 4.0;
+        Double b = 6.0;
+        Calculator c = new Calculator();
 
+        Double result = c.add(a, b);
+
+        Double expected = 10.0;
+        assertEquals(expected,  result, 0.01);
+    }
+
+    @Test
+    public void add2() {
+        Double a = 5.0;
+        Double b = -4.0;
+        Calculator c = new Calculator();
+
+        Double result = c.add(a, b);
+
+        Double expected = 1.0;
+        assertEquals(expected,  result, 0.01);
+    }
+
+    @Test
+    public void add3() {
+        Double a = -5.0;
+        Double b = -4.0;
+        Calculator c = new Calculator();
+
+        Double result = c.add(a, b);
+
+        Double expected = -9.0;
+        assertEquals(expected,  result, 0.01);
     }
 
     @Test
     public void subtract() {
+        Double a = 6.0;
+        Double b = 2.0;
+        Calculator c = new Calculator();
+
+        Double result = c.subtract(a, b);
+
+        Double expected = 4.0;
+        assertEquals(expected,  result, 0.01);
+    }
+
+    @Test
+    public void subtract2() {
+        Double a = 6.0;
+        Double b = -5.0;
+        Calculator c = new Calculator();
+
+        Double result = c.subtract(a, b);
+
+        Double expected = 11.0;
+        assertEquals(expected,  result, 0.01);
+    }
+
+    @Test
+    public void subtract3() {
+        Double a = -6.0;
+        Double b = -5.0;
+        Calculator c = new Calculator();
+
+        Double result = c.subtract(a, b);
+
+        Double expected = -1.0;
+        assertEquals(expected,  result, 0.01);
     }
 
     @Test
     public void multiply() {
+        Double a = 2.0;
+        Double b = 3.0;
+        Calculator c = new Calculator();
+
+        Double result = c.multiply(a, b);
+
+        Double expected = 6.0;
+        assertEquals(expected,  result, 0.01);
+    }
+
+    @Test
+    public void multiply2() {
+        Double a = -2.0;
+        Double b = 3.0;
+        Calculator c = new Calculator();
+
+        Double result = c.multiply(a, b);
+
+        Double expected = -6.0;
+        assertEquals(expected,  result, 0.01);
+    }
+
+    @Test
+    public void multiply3() {
+        Double a = -2.0;
+        Double b = -3.0;
+        Calculator c = new Calculator();
+
+        Double result = c.multiply(a, b);
+
+        Double expected = 6.0;
+        assertEquals(expected,  result, 0.01);
     }
 
     @Test
     public void sqrt() {
+        Double a = 4.0;
+        Calculator c = new Calculator();
+
+        Double result = c.sqrt(a);
+
+        Double expected = 2.0;
+        assertEquals(expected,  result, 0.01);
+    }
+
+    @Test
+    public void sqrt2() {
+        Double a = -4.0;
+        Calculator c = new Calculator();
+
+        Double result = c.sqrt(a);
+
+        assertEquals(new Double("NaN"), result);
+    }
+
+    @Test
+    public void sqrt3() {
+        Double a = 7.0;
+        Calculator c = new Calculator();
+
+        Double result = c.sqrt(a);
+
+        Double expected = 2.64;
+        assertEquals(expected,  result, 0.01);
     }
 
     @Test
     public void sq() {
+        Double a = 2.0;
+        Calculator c = new Calculator();
+
+        Double result = c.sq(a);
+
+        Double expected = 4.0;
+        assertEquals(expected, result, 0.01);
+    }
+
+    @Test
+    public void sq2() {
+        Double a = -3.0;
+        Calculator c = new Calculator();
+
+        Double result = c.sq(a);
+
+        Double expected = 9.0;
+        assertEquals(expected, result, 0.01);
+
     }
 
     @Test
     public void exp() {
+        Double a = 2.0;
+        Double b = 3.0;
+        Calculator c = new Calculator();
+
+        Double result = c.exp(a, b);
+
+        Double expected = 8.0;
+        assertEquals(expected, result, 0.01);
+    }
+
+    @Test
+    public void exp2() {
+        Double a = -2.0;
+        Double b = 3.0;
+        Calculator c = new Calculator();
+
+        Double result = c.exp(a, b);
+
+        Double expected = -8.0;
+        assertEquals(expected, result, 0.01);
+    }
+
+    @Test
+    public void exp3() {
+        Double a = -2.0;
+        Double b = -3.0;
+        Calculator c = new Calculator();
+
+        Double result = c.exp(a, b);
+
+        Double expected = -0.125;
+        assertEquals(expected, result, 0.01);
+    }
+
+    @Test
+    public void exp4() {
+        Double a = 2.5;
+        Double b = 3.0;
+        Calculator c = new Calculator();
+
+        Double result = c.exp(a, b);
+
+        Double expected = 15.625;
+        assertEquals(expected, result, 0.01);
     }
 
     @Test
     public void inv() {
+        Double a = 2.0;
+        Calculator c = new Calculator();
+
+        Double result = c.inv(a);
+
+        Double expected = .5;
+        assertEquals(expected, result, 0.01);
+    }
+
+    @Test
+    public void inv2() {
+        Double a = -2.0;
+        Calculator c = new Calculator();
+
+        Double result = c.inv(a);
+
+        Double expected = -0.5;
+        assertEquals(expected, result, 0.01);
+    }
+
+    @Test
+    public void inv3() {
+        Double a = .7;
+        Calculator c = new Calculator();
+
+        Double result = c.inv(a);
+
+        Double expected = 1.4285714285714286;
+        assertEquals(expected, result, 0.01);
     }
 
     @Test
     public void negate() {
+        Double a = 2.0;
+        Calculator c = new Calculator();
+
+        Double result = c.negate(a);
+
+        Double expected = -2.0;
+        assertEquals(expected, result, 0.01);
     }
+
+    @Test
+    public void negate2() {
+        Double a = -2.0;
+        Calculator c = new Calculator();
+
+        Double result = c.negate(a);
+
+        Double expected = 2.0;
+        assertEquals(expected, result, 0.01);
+    }
+
 }
