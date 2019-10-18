7e746e65fd9b3ca180bfeec4a3d7fb176e897e6e
deltagphys <cty.jgates@gmail.com>
2019-10-18 19:07:35 -0400

    Pom lang level setting, some calculator stubs

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -2,7 +2,60 @@ package com.zipcodewilmington.scientificcalculator;
 
 public class Calculator {
 
-    public Calculator() {
-        
+    private Double state;
+    private Double lastImport;
+    private Memory memory;
+    private Trig trig;
+
+
+    public void Calculator() {
+
+        this.state = 0;
+        this.lastImport = 0;
+        this.memory = new Memory();
+        this.trig = new Trig();
+
+    }
+
+    // Getters
+
+    public Double getState() {
+        return state;
+    }
+
+    public Double getLastImport() {
+        return lastImport;
+    }
+
+    // Setters
+
+    public void setState(Double state) {
+        this.state = state;
+    }
+
+    public void setLastImport(Double lastImport) {
+        this.lastImport = lastImport;
+    }
+
+    // Helper Methods
+
+
+    private void throwError() {
+
+    }
+
+    private void clearCalculator() {
+
+    }
+
+    // Input Methods
+
+    public void inputLoop() {
+        Double input = Console.getDoubleInput("");
+        Console.println(input);
+    }
+
+    public void handleOperator() {
+
     }
 }
