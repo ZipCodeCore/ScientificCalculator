5c38535434a9e9d634ec9b7d12b3f8c7c1726e52
Ron Duwell <ronduwell@zipcoders-MacBook-Pro.local>
2019-10-19 15:43:03 -0400

    Finished Squared

--- src/main/java/com/zipcodewilmington/scientificcalculator/Multiplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Multiplication.java
@@ -1,10 +1,5 @@
 package com.zipcodewilmington.scientificcalculator;
 
-/**
- * Multiplication in our scientific calculator
- * @author ronduwell
- * Oct 18, 2019
- */
 
 public class Multiplication{
     private double value1;
---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Squared.java
@@ -0,0 +1,11 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class Squared{
+    double value1;
+
+    public Squared(){
+    }
+    public Double squared(double value1) {
+        return value1 * value1;
+    }
+}
