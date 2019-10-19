33b0c9517fc46804d7cffdd727df75f5e5cec1f5
Ryan <ryan@rmiller.dev>
2019-10-18 23:19:04 -0400

    Created blank template for trigFunctions class

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/TrigFunctions.java
@@ -0,0 +1,27 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class TrigFunctions {
+
+    trigMode mode;
+
+    enum trigMode { DEGREES, RADIANS; }
+
+    //Calculate the sine of the displayed value and display it
+    public double sine()  { return 0.0; }
+
+    //Calculate the cosine of the displayed value and display it
+    public double cosine() { return 0.0; }
+
+    //Calculate the tangent of the displayed value and display it
+    public double tangent() { return 0.0; }
+
+    //Calculate the inverse sine of the displayed value and display it
+    public double inverseSine() { return 0.0; }
+
+    //Calculate the inverse cosine of the displayed value and display it
+    public double inverseCosine() { return 0.0; }
+
+    //Calculate the inverse tangent of the displayed value and display it
+    public double inverseTangent() { return 0.0; }
+
+}
