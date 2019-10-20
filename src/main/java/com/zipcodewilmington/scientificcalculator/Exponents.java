571f61b5a2592fe4a8d17bafc7c7f8550c2901b3
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-10.local>
2019-10-20 14:51:01 -0400

    Added exponents test , Completed Trig Test

--- src/main/java/com/zipcodewilmington/scientificcalculator/Exponents.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Exponents.java
@@ -5,21 +5,25 @@ public class Exponents {
 
     public static Double ExponentsMethod(Double baseNum, Double exp1) {
         Double output = Math.pow(baseNum, exp1);
+        Console.println( "\n" + output);
         return output;
     }
 
     public static Double square(Double baseNum) {
         Double output = Math.pow(baseNum, 2);
+        Console.println( "\n" + output);
         return output;
     }
 
     public static Double cubed(Double baseNum) {
         Double output = Math.pow(baseNum, 3);
+        Console.println( "\n" + output);
         return output;
     }
 
     public static Double sqrt(Double baseNum) {
         Double output = Math.sqrt(baseNum);
+        Console.println( "\n" + output);
         return output;
     }
 }
