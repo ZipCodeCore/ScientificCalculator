e18b9fb190e0edea2b60b30f52c8e5eecae425fe
Kane Kopetski <kane@zipcoders-MacBook-Pro-8.local>
2019-10-19 22:14:17 -0400

    Added Intro and ErrorMessages classes. Created user menu. Added validation methods.

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ErrorMessages.java
@@ -0,0 +1,24 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class ErrorMessages {
+
+    public static String errorMessage(Double d, Double d2, String s1) {
+
+        if(d2 == 0 && s1.equalsIgnoreCase("divide")) {
+            Console.println("You cannot divide by 0. \n");
+            Intro.intro();
+
+        } else if (d <0 && s1.equalsIgnoreCase("4")) {
+            Console.println("You cannot find the square root of a negative number.");
+
+        }
+
+        return null;
+    }
+
+    public static void invalidSelection() {
+        Console.println("Invalid option. Please try again.");
+    }
+
+
+}
---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Intro.java
@@ -0,0 +1,92 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class Intro {
+
+    public static String intro() {
+        Console.println("\nWelcome to my calculator! \n");
+
+        String s = Console.getStringInput(
+                "What would you like to do? \n" +
+                        "1. Arithmetic \n" +
+                        "2. Trigonometry \n" +
+                        "3. Exponents \n" +
+                        "4. Exit \n");
+
+        if (s.equalsIgnoreCase("1")) {
+            String s1 = Console.getStringInput(
+                    "Enter an operation \n" +
+                            "1. Add \n" +
+                            "2. Subtract \n" +
+                            "3. Multiply \n" +
+                            "4. Divide \n");
+
+         /*   if (s1.equalsIgnoreCase("1")) {
+                Double d = Console.getDoubleInput("Enter a double.");
+                Double d2 = Console.getDoubleInput("Enter another double.");
+                Arithmetic.addition(d, d2);
+                Intro.intro();
+
+            } else if (s1.equalsIgnoreCase("2")) {
+                Double d = Console.getDoubleInput("Enter a double.");
+                Double d2 = Console.getDoubleInput("Enter another double.");
+                Arithmetic.subtraction(d, d2);
+                Intro.intro();
+
+            } else if (s1.equalsIgnoreCase("3")) {
+                Double d = Console.getDoubleInput("Enter a double.");
+                Double d2 = Console.getDoubleInput("Enter another double.");
+                Arithmetic.multiply(d, d2);
+                Intro.intro();
+
+            } else if (s1.equalsIgnoreCase("4")) {
+                Double d = Console.getDoubleInput("Enter a double.");
+                Double d2 = Console.getDoubleInput("Enter another double.");
+                ErrorMessages.errorMessage(d2, s1);
+                Arithmetic.divide(d, d2);
+                Intro.intro();
+
+            }
+        } else if (s.equalsIgnoreCase("2")) {
+            String s1 = Console.getStringInput(
+                    "Enter an operation \n" +
+                            "1. Tangent \n");
+            if (s1.equalsIgnoreCase("1")) {
+                Double d = Console.getDoubleInput("Enter a double.");
+                Trigonometry.tangent(d);
+                Intro.intro();
+            }
+        } else if (s.equalsIgnoreCase("3")) {
+            String s1 = Console.getStringInput(
+                    "Enter an operation \n" +
+                            "1. Square \n" +
+                            "2. Square Root \n" +
+                            "3. Exponent \n");
+
+            if (s1.equalsIgnoreCase("1")) {
+                Double d = Console.getDoubleInput("Enter a double.");
+                Double d2 = Console.getDoubleInput("Enter another double.");
+                Exponents.square(d);
+                Intro.intro();
+
+            } else if (s1.equalsIgnoreCase("2")) {
+                Double d = Console.getDoubleInput("Enter a double.");
+                Double d2 = Console.getDoubleInput("Enter another double.");
+                Exponents.squareRoot(d);
+                Intro.intro();
+
+            } else if (s1.equalsIgnoreCase("3")) {
+                Double d = Console.getDoubleInput("Enter a double.");
+                Double d2 = Console.getDoubleInput("Enter another double.");
+                Exponents.exponent(d, d2);
+                Intro.intro();
+            }
+        } else if (s.equalsIgnoreCase("4")) {
+            System.exit(0);
+
+        } else {
+            ErrorMessages.invalidSelection();
+            Intro.intro();
+       */ }
+        return null;
+    }
+}
