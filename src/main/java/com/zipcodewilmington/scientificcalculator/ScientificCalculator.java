97430192e8a4803043fbe2b07156111fd8e3b16e
val <valfragier@gmail.com>
2019-10-20 17:11:15 -0400

    updated methods

--- src/main/java/com/zipcodewilmington/scientificcalculator/ScientificCalculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ScientificCalculator.java
@@ -17,7 +17,7 @@ public class ScientificCalculator {
     public void square(Double x){
         try {
 
-            if (x == 0) {                                              //Checks if there's already a number to work with
+            if (x == 0) {
                 x = Console.getDoubleInput("Square: ");
             }
             x = Console.getDoubleInput(x + " * ");
@@ -53,7 +53,7 @@ public class ScientificCalculator {
     public void inverse(Double x){
         try {
             if (x == 0) {
-                x = Console.getDoubleInput("First number: ");
+                x = Console.getDoubleInput("Inverse: ");
             }
 
             Double y = Console.getDoubleInput(x + " * ");
@@ -69,17 +69,18 @@ public class ScientificCalculator {
 
     public void switchSign(Double x){
         try {
-            if (x == 0) {
-                x = Console.getDoubleInput("First number: ");
+            x = x * -1;
+
+            if (x > 0) {
+                x = Console.getDoubleInput("Switch Sign: ");
             }
 
-            Double y = Console.getDoubleInput(x + " / ");
-            while(y == 0){
-                y = Console.getDoubleInput("Please enter a non-zero denominator: ");
+            while(x == 0){
+                x = Console.getDoubleInput("Please enter a non-zero denominator: ");
             }
 
-            this.setDoubleResult(x / y);
-            this.setStringResult(Double.toString(x / y));
+            this.setDoubleResult(x * -1);
+            this.setStringResult(Double.toString(x * -1));
 
         }
         //for non double inputs and division by zero.
@@ -90,82 +91,90 @@ public class ScientificCalculator {
 
     public void sine(Double x){
         try {
-            if (x == 0) {
-                x = Console.getDoubleInput("First number: ");
+            if (x > 0) {
+                x = Console.getDoubleInput("Sine: ");
             }
 
-            Double y = Console.getDoubleInput(x + " / ");
-            while(y == 0){
-                y = Console.getDoubleInput("Please enter a non-zero denominator: ");
+            while(x == 0){
+                x = Console.getDoubleInput("Please enter a non-zero denominator: ");
             }
 
-            this.setDoubleResult(x / y);
-            this.setStringResult(Double.toString(x / y));
+            this.setDoubleResult(Math.sin(x));
+            this.setStringResult(Double.toString(Math.sin(x)));
 
         }
-        //for non double inputs and division by zero.
-        catch(Exception e){
+        //for non double inputs
+        catch (Exception e) {
+            this.setDoubleResult(null);
             this.setStringResult("ERROR");
         }
     }
 
     public void cosine(Double x){
         try {
-            if (x == 0) {
-                x = Console.getDoubleInput("First number: ");
+            if (x > 0) {
+                x = Console.getDoubleInput("Cosine: ");
             }
 
-            Double y = Console.getDoubleInput(x + " / ");
-            while(y == 0){
-                y = Console.getDoubleInput("Please enter a non-zero denominator: ");
+            while(x == 0){
+                x = Console.getDoubleInput("Please enter a non-zero denominator: ");
             }
 
-            this.setDoubleResult(x / y);
-            this.setStringResult(Double.toString(x / y));
+            this.setDoubleResult(Math.cos(x));
+            this.setStringResult(Double.toString(Math.cos(x)));
 
         }
-        //for non double inputs and division by zero.
-        catch(Exception e){
+        //for non double inputs
+        catch (Exception e) {
+            this.setDoubleResult(null);
             this.setStringResult("ERROR");
         }
+
     }
 
 
-    public void tangent(Double x){
+    public void tangent(Double x) {
         try {
-            if (x == 0) {
-                x = Console.getDoubleInput("First number: ");
+
+            if (x > 0) {
+                x = Console.getDoubleInput("Tangent: ");
             }
 
-            Double y = Console.getDoubleInput(x + " / ");
-            while(y == 0){
-                y = Console.getDoubleInput("Please enter a non-zero denominator: ");
+            while(x == 0){
+                x = Console.getDoubleInput("Please enter a non-zero denominator: ");
             }
 
-            this.setDoubleResult(x / y);
-            this.setStringResult(Double.toString(x / y));
+                this.setDoubleResult(Math.tan(x));
+                this.setStringResult(Double.toString(Math.tan(x)));
 
-        }
-        //for non double inputs and division by zero.
-        catch(Exception e){
-            this.setStringResult("ERROR");
-        }
+            }
+            //for non double inputs
+            catch (Exception e) {
+                this.setDoubleResult(null);
+                this.setStringResult("ERROR");
+            }
+
     }
 
     public void inverseSine(Double x){
         try {
-            if (x == 0) {
-                x = Console.getDoubleInput("First number: ");
+            double degrees = x;
+            double radians = Math.toRadians(degrees);
+            double sin;
+
+            sin = Math.sin(radians);
+
+
+            if (x > 0) {
+                x = Console.getDoubleInput("Inverse Sine: ");
             }
 
-            Double y = Console.getDoubleInput(x + " / ");
-            while(y == 0){
-                y = Console.getDoubleInput("Please enter a non-zero denominator: ");
+            while(x == 0){
+                x = Console.getDoubleInput("Please enter a non-zero denominator: ");
             }
 
-            this.setDoubleResult(x / y);
-            this.setStringResult(Double.toString(x / y));
-
+            this.setDoubleResult(Math.toDegrees(Math.asin(sin)));
+            this.setStringResult(Double.toString(Math.toDegrees(Math.asin(sin))));
         }
         //for non double inputs and division by zero.
         catch(Exception e){
@@ -174,40 +183,51 @@ public class ScientificCalculator {
     }
 
     public void inverseCosine(Double x){
-        try {
-            if (x == 0) {
-                x = Console.getDoubleInput("First number: ");
+            try {
+
+                double degrees = x;
+                double radians = Math.toRadians(degrees);
+                double cos;
+
+                cos = Math.cos(radians);
+
+                if (x > 0) {
+                    x = Console.getDoubleInput("Inverse Cosine: ");
+                }
+
+                while(x == 0){
+                    x = Console.getDoubleInput("Please enter a non-zero denominator: ");
+                }
+
+                this.setDoubleResult(Math.toDegrees(Math.acos(cos)));
+                this.setStringResult(Double.toString(Math.toDegrees(Math.acos(cos))));
+            }
+            //for non double inputs and division by zero.
+            catch(Exception e){
+                this.setStringResult("ERROR");
             }
 
-            Double y = Console.getDoubleInput(x + " / ");
-            while(y == 0){
-                y = Console.getDoubleInput("Please enter a non-zero denominator: ");
-            }
-
-            this.setDoubleResult(x / y);
-            this.setStringResult(Double.toString(x / y));
-
-        }
-        //for non double inputs and division by zero.
-        catch(Exception e){
-            this.setStringResult("ERROR");
-        }
     }
 
     public void inverseTangent(Double x){
         try {
-            if (x == 0) {
-                x = Console.getDoubleInput("First number: ");
+            double degrees = x;
+            double radians = Math.toRadians(degrees);
+            double tan;
+
+            tan = Math.tan(radians);
+
+
+            if (x > 0) {
+                x = Console.getDoubleInput("Inverse Tangent: ");
             }
 
-            Double y = Console.getDoubleInput(x + " / ");
-            while(y == 0){
-                y = Console.getDoubleInput("Please enter a non-zero denominator: ");
+            while(x == 0){
+                x = Console.getDoubleInput("Please enter a non-zero denominator: ");
             }
 
-            this.setDoubleResult(x / y);
-            this.setStringResult(Double.toString(x / y));
-
+            this.setDoubleResult(Math.toDegrees(Math.atan(tan)));
+            this.setStringResult(Double.toString(Math.toDegrees(Math.atan(tan))));
         }
         //for non double inputs and division by zero.
         catch(Exception e){
@@ -229,9 +249,10 @@ public class ScientificCalculator {
                 fact = fact * i;
             }
 
+            // Correct the output
             x = Console.getDoubleInput(x + " ! ");
-            this.setDoubleResult(fact * i);
-            this.setStringResult(Double.toString(x!));
+            this.setDoubleResult(x * fact);
+            this.setStringResult(Double.toString(x * fact));
 
         }
         //for non double inputs
