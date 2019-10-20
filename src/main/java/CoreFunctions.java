7ec964a7ce6282a43babf9d410c81191516c5a5f
mike <mike.schoenbeck@gmail.com>
2019-10-20 12:00:56 -0400

    All core functions and methods saved

--- src/main/java/CoreFunctions.java
+++ src/main/java/CoreFunctions.java
@@ -1,6 +1,6 @@
 public class CoreFunctions {
 
-    private double num1;
+    /*private double num13;
     private double num2;
     private double answer;
 
@@ -14,7 +14,7 @@ public class CoreFunctions {
 
     /*public void setAnswer(double answer) {
         this.answer = answer;
-    }*/
+    }
 
     public double getNum1() {
         return num1;
@@ -29,20 +29,43 @@ public class CoreFunctions {
     }*/
 
     public double add(double num1, double num2) {
-        return this.answer = num1 + num2;
+        return num1 + num2;
     }
 
     public double subtract(double num1, double num2)    {
-        return this.answer = num1 - num2;
+        return num1 - num2;
     }
 
     public double multiply(double num1, double num2)    {
-        return this.answer = num1 * num2;
+        return  num1 * num2;
     }
 
     public double divide(double num1, double num2)  {
-        return this.answer = num1 / num2;
+        return  num1 / num2;
     }
+
+    public double power(double num1, double num2) {
+        return Math.pow(num1, num2);
+    }
+
+    public double square(double num1)   {
+        return Math.pow(num1, 2);
+    }
+
+    public double squareRt(double num1)    {
+        return Math.sqrt(num1);
+    }
+
+    public double invertSign(double num1)  {
+        return /*this.answer =*/ num1 / -1;
+    }
+
+    public double inverse(double num1) {
+        return /*this.answer =*/ num1 / -1;
+    }
+
+
+
 
 
 
--- src/test/java/TestCoreFunctions.java
+++ src/test/java/TestCoreFunctions.java
@@ -2,44 +2,86 @@ import org.junit.Assert;
 
 public class TestCoreFunctions {
 
-@org.junit.Before
-    public void setUp() throws Exception{
+    @org.junit.Before
+    public void setUp() throws Exception {
 
-}
-@org.junit.After
-    public void tearDown() throws Exception{
+    }
 
-}
+    @org.junit.After
+    public void tearDown() throws Exception {
 
-@org.junit.Test
-    public void add()   {
-    CoreFunctions add1 = new CoreFunctions();
-    Assert.assertEquals(3., add1.add(1.,2.), 0.00);
-    Assert.assertEquals(100., add1.add(25., 75.), 0.00);
-    Assert.assertEquals(1000., add1.add(550., 450.), 0.00);
-}
-
-@org.junit.Test
-    public void subtract()  {
-    CoreFunctions subtract1 = new CoreFunctions();
-    Assert.assertEquals(1., subtract1.subtract(2.,1.), 0.00);
-    Assert.assertEquals(50., subtract1.subtract(250., 200.), 0.00);
-    Assert.assertEquals(100., subtract1.subtract(102.,2.), 0.00);
-}
-
-@org.junit.Test
-    public void multiply()  {
-    CoreFunctions multiply1 = new CoreFunctions();
-    Assert.assertEquals(10, multiply1.multiply(5, 2), 0.00);
-    Assert.assertEquals(100, multiply1.multiply(10,10), 0.00);
-    Assert.assertEquals(1000, multiply1.multiply(1000,1), 0.00);
-}
+    }
 
     @org.junit.Test
-    public void divide()  {
-    CoreFunctions divide1 = new CoreFunctions();
-    Assert.assertEquals(5, divide1.divide(25,5), 0.00);
-    Assert.assertEquals(10, divide1.divide(100, 10), 0.00);
-    Assert.assertEquals(25, divide1.divide(100, 4), 0.00);
+    public void add() {
+        CoreFunctions add1 = new CoreFunctions();
+        Assert.assertEquals(3., add1.add(1., 2.), 0.00);
+        Assert.assertEquals(100., add1.add(25., 75.), 0.00);
+        Assert.assertEquals(1000., add1.add(550., 450.), 0.00);
     }
-}
+
+    @org.junit.Test
+    public void subtract() {
+        CoreFunctions subtract1 = new CoreFunctions();
+        Assert.assertEquals(1., subtract1.subtract(2., 1.), 0.00);
+        Assert.assertEquals(50., subtract1.subtract(250., 200.), 0.00);
+        Assert.assertEquals(100., subtract1.subtract(102., 2.), 0.00);
+    }
+
+    @org.junit.Test
+    public void multiply() {
+        CoreFunctions multiply1 = new CoreFunctions();
+        Assert.assertEquals(10, multiply1.multiply(5, 2), 0.00);
+        Assert.assertEquals(100, multiply1.multiply(10, 10), 0.00);
+        Assert.assertEquals(1000, multiply1.multiply(1000, 1), 0.00);
+    }
+
+    @org.junit.Test
+    public void divide() {
+        CoreFunctions divide1 = new CoreFunctions();
+        Assert.assertEquals(5, divide1.divide(25, 5), 0.00);
+        Assert.assertEquals(10, divide1.divide(100, 10), 0.00);
+        Assert.assertEquals(25, divide1.divide(100, 4), 0.00);
+    }
+
+    @org.junit.Test
+    public void power() {
+        CoreFunctions power1 = new CoreFunctions();
+        Assert.assertEquals(4, power1.power(2, 2), 0.00);
+        Assert.assertEquals(27, power1.power(3, 3), 0.00);
+        Assert.assertEquals(10000, power1.power(10, 4), 0.00);
+    }
+
+    @org.junit.Test
+    public void square() {
+        CoreFunctions square1 = new CoreFunctions();
+        Assert.assertEquals(4, square1.square(2), 0.00);
+        Assert.assertEquals(64, square1.square(-8), 0.00);
+        Assert.assertEquals(81, square1.square(9), 0.00);
+    }
+
+    @org.junit.Test
+    public void squareRt() {
+        CoreFunctions square1 = new CoreFunctions();
+        Assert.assertEquals(4, square1.squareRt(16), 0.00);
+        Assert.assertEquals(2, square1.squareRt(4), 0.00);
+        Assert.assertEquals(8, square1.squareRt(64), 0.00);
+        // cannot get square root of negative number. needs condition.
+    }
+
+    @org.junit.Test
+    public void invertSign() {
+        CoreFunctions invert1 = new CoreFunctions();
+        Assert.assertEquals(-10, invert1.invertSign(10), 0.00);
+        Assert.assertEquals(10, invert1.invertSign(-10), 0.00);
+        Assert.assertEquals(27, invert1.invertSign(-27), 0.00);
+    }
+
+    @org.junit.Test
+    public void inverse() {
+        CoreFunctions inverse1 = new CoreFunctions();
+        Assert.assertEquals(-1, inverse1.inverse(1), 0.00);
+        Assert.assertEquals(10, inverse1.inverse(-10), 0.00);
+        Assert.assertEquals(-.005, inverse1.inverse(.005), 0.00);
+    }
+    }
