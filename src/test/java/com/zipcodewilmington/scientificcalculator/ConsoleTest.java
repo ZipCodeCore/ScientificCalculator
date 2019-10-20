f4d06a7dcb5e09c745b9f4fd10a72aadb288cf9b
deltagphys <cty.jgates@gmail.com>
2019-10-20 15:30:30 -0400

    most displaymode tests

--- src/test/java/com/zipcodewilmington/scientificcalculator/ConsoleTest.java
+++ src/test/java/com/zipcodewilmington/scientificcalculator/ConsoleTest.java
@@ -5,11 +5,81 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ConsoleTest {
+
+    @Test
+
+    public void printNumTest() {
+        Calculator calculator = new Calculator();
+        calculator.setDisplayMode(Calculator.dMode.BINARY);
+        calculator.setDisplay(64.0);
+        String expected = "1000000";
+        String actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+        calculator.setDisplay(13.0);
+        expected = "1101";
+        actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+        calculator.setDisplay(13.3);
+        expected = "1101";
+        actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+        calculator.setDisplayMode(Calculator.dMode.OCTAL);
+        calculator.setDisplay(64.0);
+        expected = "100";
+        actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+        calculator.setDisplay(13.0);
+        expected = "15";
+        actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+        calculator.setDisplay(13.3);
+        expected = "15";
+        actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+        calculator.setDisplayMode(Calculator.dMode.HEXADECIMAL);
+        calculator.setDisplay(64.0);
+        expected = "40";
+        actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+        calculator.setDisplay(13.0);
+        expected = "D";
+        actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+        calculator.setDisplay(13.3);
+        expected = "D";
+        actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+        calculator.setDisplayMode(Calculator.dMode.DECIMAL);
+        calculator.setDisplay(64.0);
+        expected = "64.0";
+        actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+        calculator.setDisplay(13.0);
+        expected = "13.0";
+        actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+        calculator.setDisplay(13.3);
+        expected = "13.3";
+        actual = Console.printNum(calculator.getDisplay(),calculator.getDisplayMode());
+        assertTrue(expected.equals(actual));
+
+    }
+
     @Test
     public void cleanInputErrors() {
         assertEquals("ERR",Console.cleanInput("blargh"));
         assertEquals("ERR",Console.cleanInput("blargh"));
-
     }
 
     @Test
