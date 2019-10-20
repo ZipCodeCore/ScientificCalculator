8a568a806c92dced773a379eb55f89448f0333a1
Ron Duwell <ronduwell@zipcoders-MacBook-Pro.local>
2019-10-20 10:24:30 -0400

    Morning updates

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -0,0 +1,4 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class Calculator {
+}
--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -12,48 +12,44 @@ public class MainApplication {
 
 
     public static void main(String[] args) {
+        boolean turnOn = true;
+        //make a list of basic commands for calulator
+        String[] basicCommands = {"run calculator", "clear display", "print current number", "switch trig units",
+                "switch display mode", "erase memory", "turn off"};
+        //turn that array into a string
+        List<String> basicCommandsList = Arrays.asList(basicCommands);
+        //call the scanner
         Scanner scanner = new Scanner(System.in);
         //welcome the user
-        System.out.println("Hey! Welcome to the calculator!\nWould you care for a drink?\n");
+        System.out.println("Hey! Welcome to the calculator!\nWould you care for a drink?\n\n");
+        while (turnOn) {
+            System.out.println("What would you like to do?\nType \"cmd\" for commands.");
+            String userCommand = scanner.nextLine();
 
-        //ask the user for a value
-        System.out.println("Enter your value");
-        String userValueAsString = scanner.nextLine();
-        Double userValue1 = Double.parseDouble(userValueAsString);
-
-        //ask the user for a operator
-        //should eventually test to see if they've entered a proper input
-        //should also eventually System.out.println a list for them to choose from
-        System.out.println("Choose your Operator");
-        String userOperator = scanner.nextLine();
-
-
-        //create an array of each operation that requires two values
-        String[] need2Values = {"add", "subtract", "multiply", "divided", "exponent"};
-        //turn that array into a string
-        List<String> need2ValuesList = Arrays.asList(need2Values);
-
-        //test the array to see if the userOperator is included
-        if (need2ValuesList.contains(userOperator)) {
-            //ask for a second value
-            System.out.println("Enter your second value");
-            String userValue2AsString = scanner.nextLine();;
-            Double  userValue2 = Double.parseDouble(userValue2AsString);
-
-            //run the need2value operators INSIDE the if statement
-            switch (userOperator) {
-                case "add":
-                    Addition add = new Addition ();
-                    double sum = add.add(userValue1, userValue2);
-                    System.out.println(userValue1 + " + " + userValue2 + " = " + sum);
+            while (!basicCommandsList.contains(userCommand)) {
+                if (userCommand.equals("cmd")) {
+                    System.out.println("*\"run calculator\" - Run the Calculator*");
+                    System.out.println("*\"turn off\" - Turn off the Calculator*");
+                    System.out.println("*Other list of commands UPCOMING!*\n");
+                } else if (basicCommandsList.contains(userCommand)) {
                     break;
-                case "subtract":
-                    Subtraction subtract = new Subtraction();
-                    double difference = subtract.subtract(userValue1, userValue2);
-                    System.out.println(userValue1 + " - " + userValue2 + " = " + difference);
+                } else {
+                    System.out.println("ERROR: Invalid command.\n");
+                }
+                System.out.println("What would you like to do?\nType \"cmd\" for commands.");
+                userCommand = scanner.nextLine();
+            }
+
+            switch (userCommand) {
+                case "run calculator":
+                    Calculator calculator = new Calculator();
+                    calculator.runCalculator();
+                    break;
+                case "turn off":
+                    turnOn = false;
                     break;
             }
         }
+        System.out.println("Goodbye! Have a nice day!");
     }
 }
-
