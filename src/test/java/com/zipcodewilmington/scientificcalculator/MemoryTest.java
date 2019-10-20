73bb2a35b07b3ed235b08f30a84ba223403229b1
Mike King <michkin32@gmail.com>
2019-10-20 11:56:42 -0400

    Finished memory test with comments

--- src/test/java/com/zipcodewilmington/scientificcalculator/MemoryTest.java
+++ src/test/java/com/zipcodewilmington/scientificcalculator/MemoryTest.java
@@ -15,40 +15,49 @@ public class MemoryTest {
     @After
     public void tearDown() throws Exception {
     }
+
     @Test
+    //Test if memory plus method changes the objects state
+
     public void Memory_state1() {
         Double expected = 25d;
 
         Memory myMemory = new Memory();
         myMemory.memoryPlus(25d);
 
-        Double actual = myMemory.memoryRecall();
+        Double actual = myMemory.state;
         assertEquals(expected, actual);
     }
 
     @Test
+    //Test if memory plus method changes the objects state more than once
+
     public void Memory_state2() {
         Double expected = 32d;
 
         Memory myMemory = new Memory();
         myMemory.memoryPlus(25d);
         myMemory.memoryPlus(32d);
-        Double actual = myMemory.memoryRecall();
+        Double actual = myMemory.state;
         assertEquals(expected, actual);
     }
 
     @Test
+    //An assertnotequals Test if memory plus method changes the objects state
+    
     public void Memory_state3() {
         Double expected = 25d;
 
         Memory myMemory = new Memory();
         myMemory.memoryPlus(25d);
         myMemory.memoryPlus(32d);
-        Double actual = myMemory.memoryRecall();
+        Double actual = myMemory.state;
         assertNotEquals(expected, actual);
     }
 
     @Test
+    //Test if memory reset method changes the objects state back to 0.0
+
     public void Memory_Reset1() {
         Double expected = 0.0d;
 
@@ -60,6 +69,8 @@ public class MemoryTest {
         assertEquals(expected, actual);
     }
     @Test
+    //Test if memory reset method changes the objects state more than once
+
     public void Memory_Reset2() {
         Double expected = 0.0d;
 
@@ -73,6 +84,8 @@ public class MemoryTest {
         assertEquals(expected, actual);
     }
     @Test
+    //assertnotequals Test if memory reset method changes the objects state
+
     public void Memory_Reset3() {
         Double expected = 32d;
 
@@ -85,5 +98,43 @@ public class MemoryTest {
         assertNotEquals(expected, actual);
     }
 
+    @Test
+    //Test if memory recall method returns the objects state
+
+    public void Memory_Recall_Test1() {
+        Double expected = 25d;
+
+        Memory myMemory = new Memory();
+        myMemory.memoryPlus(25d);
+
+        Double actual = myMemory.memoryRecall();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    //Test if memory recall method returns the objects state after its been changed twice
+
+    public void Memory_Recall_Test2() {
+        Double expected = 32d;
+
+        Memory myMemory = new Memory();
+        myMemory.memoryPlus(25d);
+        myMemory.memoryPlus(32d);
+        Double actual = myMemory.memoryRecall();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    //assertnotequals Test if memory recall method returns the objects state after its been changed twice
+
+    public void Memory_Recall_Test3() {
+        Double expected = 25d;
+
+        Memory myMemory = new Memory();
+        myMemory.memoryPlus(25d);
+        myMemory.memoryPlus(32d);
+        Double actual = myMemory.memoryRecall();
+        assertNotEquals(expected, actual);
+    }
 }
 
