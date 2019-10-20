a8c4012b93fca0de9c7d2797b6e8edf6aea6afa8
Ben Coon <bencoon@zipcoders-MacBook-Pro-22.local>
2019-10-20 18:28:54 -0400

    Working Simple Calc

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -42,36 +42,52 @@ public class MainApplication {
                 System.out.println(num1 + " / " + num2 + " = " + result);
                 break;
             case 5:
+                simpleMath.square(num1);
                 System.out.println(num1 + " squared is " + result);
                 break;
             case 6:
                 num2 = Console.getDoubleInput("Enter your second number.");
-                //simpleMath.exponent(num1, num2);
+                simpleMath.expo(num1, num2);
                 System.out.println(num1 + " to the power of " + num2 + " = " + result);
                 break;
             case 7:
+                simpleMath.squareRoot(num1);
                 System.out.println("The square root of " + num1 + " is " + result);
                 break;
             case 8:
+                simpleMath.inverse(num1);
                 System.out.println("The inverse of " + num1 + " is " + result);
                 break;
             case 9:
+                simpleMath.invertSign(num1);
+                System.out.println("The invert of " + num1 + " is " + result);
+            case 10:
                 System.out.println("The logarithm of " + num1 + " is " + result);
                 break;
-            case 10:
+            case 11:
                 System.out.println("The sine of " + num1 + " is " + result);
                 break;
-            case 11:
+            case 12:
                 System.out.println("The cosine of " + num1 + " is " + result);
                 break;
-            case 12:
+            case 13:
                 System.out.println("The tangent of " + num1 + " is " + result);
+                break;
+            case 14:
+                System.out.println("The arcsine of " + num1 + " is " + result);
+                break;
+            case 15:
+                System.out.println("The arccosine of " + num1 + " is " + result);
+                break;
+            case 16:
+                System.out.println("The arctangent of " + num1 + " is " + result);
                 break;
             default:
                 System.out.println("Your input was incorrect please type one of the following:\n" +
                         "1 for addition \n2 for subtraction\n3 for multiplication\n4 for division\n" +
                         "5 for squaring \n6 for exponent\n7 for square root\n8 for inverse\n" +
-                        "9 for invert\n10 for logarithms\n11 for sine\n12 for cosine\ntan for tangent\n");
+                        "9 for invert\n10 for logarithms\n11 for sine\n12 for cosine\n13 tan for tangent\n" +
+                        "14 for arcsine\n15 for arccosine\n16 for arctangent");
                 break;
 
 
