afaa2f17dea59ac0ad3423be6506ecd59df7f998
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-10-20 18:31:35 -0400

    Replaced Intro class with MainMenu SubMenu and Operations classes

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -6,6 +6,6 @@ package com.zipcodewilmington.scientificcalculator;
 public class MainApplication {
     public static void main(String[] args) {
 
-        Intro.intro();
+        MainMenu.mainMenu();
     }
 }
