e2805911ee05b3608d8ac9f9b622bfbb8456cef0
Kai Shields <kai@zipcoders-MacBook-Pro-3.local>
2019-10-20 22:48:22 -0400

    added for master

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -20,34 +20,33 @@ public class MainApplication {
 
         while (turnOn) {
             try {
-
                 CoreMethods basic = new CoreMethods();
-                double inputValue = Console.getDoubleInput("\n\nWhat can I help you with Today? \nChoose: \n1 - Mulitply \n"
-                        + "2 - Square \n" + "3 - Power  \n" + "4 - Division \n" + "5 - Square Root \n" + "6 - Inverse \n" +
-                        "7 - Add \n" + "8 - Subtract \n" +"9 - Neg or Pos \n" + "10 - Turn off !");
+                double inputValue = Console.getDoubleInput("\n\nWhat can I help you with Today? \nChoose: \n1 - Addition \n"
+                        + "2 - Subtraction \n" + "3 - Multiply  \n" + "4 - Division \n" + "5 - Square \n" + "6 - Power \n" +
+                        "7 - Square Root \n" + "8 - Inverse \n" +"9 - Neg or Pos \n" + "10 - Turn off !");
 
                 if (inputValue > 10) {
                     System.out.println("Error : Hey don't do that! ");
                     turnOn = false;
                 }
-
                 if (inputValue <= 0) {
                     System.out.println("Error : Hey don't do that! ");
                     turnOn = false;
                 } else if (inputValue == 1) {
+                    System.out.println("Addition Time!");
+                    double a = Console.getDoubleInput("Enter your number:");
+                    double b = Console.getDoubleInput("Enter your number:");
+                    System.out.println(basic.calcAdd(a, b));
+                } else if (inputValue == 2) {
+                    System.out.println("Subtraction Time!");
+                    double a = Console.getDoubleInput("Enter your number:");
+                    double b = Console.getDoubleInput("Enter your number:");
+                    System.out.println(basic.calcSub(a, b));
+                }else if (inputValue == 3) {
                     System.out.println("Multiply Time! ");
                     double a = Console.getDoubleInput("Enter your number:");
                     double b = Console.getDoubleInput("Enter your number::");
                     System.out.println(basic.calcMultiply(a, b));
-                } else if (inputValue == 2) {
-                    System.out.println("Square Time!");
-                    double a = Console.getDoubleInput("Enter your number:");
-                    System.out.println(basic.calcSquared(a));
-                } else if (inputValue == 3) {
-                    System.out.println("Power Time!");
-                    double a = Console.getDoubleInput("Enter your number:");
-                    double b = Console.getDoubleInput("Enter your number:");
-                    System.out.println(basic.calcToPower(a, b));
                 } else if (inputValue == 4) {
                     System.out.println("Division Time!");
                     double a = Console.getDoubleInput("Enter your number:");
@@ -59,25 +58,23 @@ public class MainApplication {
                     } else {
                         System.out.println(basic.calcDiv(a, b));
                     }
-
-                } else if (inputValue == 5) {
+                }else if (inputValue == 5) {
+                    System.out.println("Square Time!");
+                    double a = Console.getDoubleInput("Enter your number:");
+                    System.out.println(basic.calcSquared(a));
+                } else if (inputValue == 6) {
+                    System.out.println("Power Time!");
+                    double a = Console.getDoubleInput("Enter your number:");
+                    double b = Console.getDoubleInput("Enter your number:");
+                    System.out.println(basic.calcToPower(a, b));
+                } else if (inputValue == 7) {
                     System.out.println("Square Root Time!");
                     double a = Console.getDoubleInput("Enter your number:");
                     System.out.println(basic.calcSqrt(a));
-                } else if (inputValue == 6) {
+                } else if (inputValue == 8) {
                     System.out.println("Inverse Time!");
                     double a = Console.getDoubleInput("Enter your number:");
                     System.out.println(basic.calcSqrt(a));
-                } else if (inputValue == 7) {
-                    System.out.println("Addition Time!");
-                    double a = Console.getDoubleInput("Enter your number:");
-                    double b = Console.getDoubleInput("Enter your number:");
-                    System.out.println(basic.calcAdd(a, b));
-                } else if (inputValue == 8) {
-                    System.out.println("Subtraction Time!");
-                    double a = Console.getDoubleInput("Enter your number:");
-                    double b = Console.getDoubleInput("Enter your number:");
-                    System.out.println(basic.calcSub(a, b));
                 } else if (inputValue == 9) {
                     System.out.println("Negative or Positive Time!");
                     double a = Console.getDoubleInput("Enter your number:");
