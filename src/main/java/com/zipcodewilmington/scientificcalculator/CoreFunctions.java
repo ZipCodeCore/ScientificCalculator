4cf0b759b40bcf317001ce27160c098cbc48a888
Giles Bradford <gbradford@zipcodes-MacBook-Pro-4.local>
2020-02-07 18:07:08 -0500

    Added empty constructor

--- src/main/java/com/zipcodewilmington/scientificcalculator/CoreFunctions.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/CoreFunctions.java
@@ -2,8 +2,13 @@ package com.zipcodewilmington.scientificcalculator;
 
 public class CoreFunctions {
 
+    //This is a constructor for CLass
+    public void CoreFunctions() {
+
+    }
+
     public class Add{
-        
+
     }
 
     public class Subtract{
