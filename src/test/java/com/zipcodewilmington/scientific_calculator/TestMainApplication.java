0ddeebc86f68d1395161e2be14c3db251f5ce87a
Jeremy McCray <jmccray@zipcodes-MacBook-Pro.local>
2020-02-07 18:18:50 -0500

    working on a test

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,18 @@
 package com.zipcodewilmington.scientific_calculator;
 
+import com.zipcodewilmington.scientificcalculator.CoreFunctions;
+import org.junit.Test;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+    CoreFunctions mathTests = new CoreFunctions();
+}
+@Test
+public class adderTest{
+
+    int assumed = 3;
+    int actual = mathTests.add();
+    assertEquals( assumed, actual);
 }
