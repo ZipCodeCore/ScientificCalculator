9321da63222b73ac939f068ea9e6ac6b3842279e
vle <obentoka@gmail.com>
2020-02-07 18:30:54 -0500

    first codes test

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Operations.java
@@ -0,0 +1,8 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class Operations {
+
+    public static long add(long a, long b){
+        return a + b;
+    }
+}
---
+++ src/test/java/com/zipcodewilmington/scientific_calculator/OperationsTest.java
@@ -0,0 +1,13 @@
+package com.zipcodewilmington.scientific_calculator;
+
+import org.junit.Test;
+
+public class OperationsTest {
+
+    @Test
+    public void addTest(){
+
+        long expected = 1000000000;
+        long actual = add(50000, 50000);
+    }
+}
