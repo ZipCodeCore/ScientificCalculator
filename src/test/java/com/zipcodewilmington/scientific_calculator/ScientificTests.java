8d631fc22af00e908e945649b5ea3634a6513bf7
Moe Aydin <maydin@zipcodes-MacBook-Pro-7.local>
2020-02-08 14:46:07 -0500

    test

--- src/test/java/com/zipcodewilmington/scientific_calculator/ScientificTests.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/ScientificTests.java
@@ -1,4 +1,29 @@
 package com.zipcodewilmington.scientific_calculator;
 
+import com.zipcodewilmington.scientificcalculator.ScientificCalc;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ScientificTests {
+
+    ScientificCalc scientific = new ScientificCalc();
+
+    // Sine Tests in Radians and Degrees
+    @Test
+    public void sinRadians() {
+        double expected = 0;
+        double actual = scientific.sin(0);
+        assertEquals(expected, actual, 0.01);
+    }
+
+    /*
+    @Test
+    public void sinRadians() {
+        double expected = 0;
+        double actual = scientific.sin(0);
+        assertEquals(expected, actual, 0.01);
+    }
+    */
+
 }
