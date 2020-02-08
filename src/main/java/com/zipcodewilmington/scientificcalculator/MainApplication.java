4e75a56084b9ca33d47f337f7906aed5ccd05781
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-08 14:45:24 -0500

    most of ui done

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -144,12 +144,15 @@ public class MainApplication {
             }
             else if (s.equals("a")) {
                 //Clear Error
+                s = "";
             }
             else if (s.equals("b")) {
                 //Clear Display
+                s = "";
             }
             else if (s.equals("c")) {
                 //Back
+                Greeting();
             }
             else if (s.equals("d")) {
                 //Main Menu
@@ -185,13 +188,219 @@ public class MainApplication {
                     else if(s.equals("4")){
                         //hex
                     }
+                    else if (s.equals("a")) {
+                        //Clear Error
+                        s = "";
+                    }
+                    else if (s.equals("b")) {
+                        //Clear Display
+                        s = "";
+                    }
+                    else if (s.equals("c")) {
+                        //Back
+                        SciMenuHome();
+                    }
+                    else if (s.equals("d")) {
+                        //Main Menu
+                        Greeting();
+                    }
+                    else if (s.equals("e")) {
+                        //Exit
+                        ExitCalc();
+                    }
                     else {
                         System.out.println("Please enter a valid response");
                     }
             }
 //End Rotate Display
+//Start Memory
             else if(s.equals("2")) {
                 Memory();
+                s = Console.getStringInput("Please select an option");
+                if(s.equals("1")) {
+                    //M+
+                }
+                else if(s.equals("2")){
+                    //MC
+                }
+                else if(s.equals("3")){
+                    //MRC
+                }
+                else if (s.equals("a")) {
+                    //Clear Error
+                    s = "";
+                }
+                else if (s.equals("b")) {
+                    //Clear Display
+                    s = "";
+                }
+                else if (s.equals("c")) {
+                    //Back
+                    SciMenuHome();
+                }
+                else if (s.equals("d")) {
+                    //Main Menu
+                    Greeting();
+                }
+                else if (s.equals("e")) {
+                    //Exit
+                    ExitCalc();
+                }
+                else {
+                    System.out.println("Please enter a valid response");
+                }
+            }
+//End Memory
+//Start Trig
+            else if(s.equals("3")) {
+                TrigFunctions();
+                s = Console.getStringInput("Please select an option");
+                if(s.equals("1")) {
+                    //sin
+                }
+                else if(s.equals("2")){
+                    //cos
+                }
+                else if(s.equals("3")){
+                    //tan
+                }
+                else if(s.equals("4")){
+                    //arcsin
+                }
+                else if(s.equals("5")){
+                    //arccos
+                }
+                else if(s.equals("6")){
+                    //arctan
+                }
+                else if (s.equals("a")) {
+                    //Clear Error
+                    s = "";
+                }
+                else if (s.equals("b")) {
+                    //Clear Display
+                    s = "";
+                }
+                else if (s.equals("c")) {
+                    //Back
+                    SciMenuHome();
+                }
+                else if (s.equals("d")) {
+                    //Main Menu
+                    Greeting();
+                }
+                else if (s.equals("e")) {
+                    //Exit
+                    ExitCalc();
+                }
+                else {
+                    System.out.println("Please enter a valid response");
+                }
+            }
+//End Trig
+//Start Mode Change
+            else if(s.equals("4")) {
+                ModeChange();
+                s = Console.getStringInput("Please select an option");
+                if(s.equals("1")) {
+                    //deg
+                }
+                else if(s.equals("2")){
+                    //rad
+                }
+                else if (s.equals("a")) {
+                    //Clear Error
+                    s = "";
+                }
+                else if (s.equals("b")) {
+                    //Clear Display
+                    s = "";
+                }
+                else if (s.equals("c")) {
+                    //Back
+                    SciMenuHome();
+                }
+                else if (s.equals("d")) {
+                    //Main Menu
+                    Greeting();
+                }
+                else if (s.equals("e")) {
+                    //Exit
+                    ExitCalc();
+                }
+                else {
+                    System.out.println("Please enter a valid response");
+                }
+            }
+//End Mode Change
+//Start Log Function
+            else if(s.equals("5")) {
+                LogFunctions();
+                s = Console.getStringInput("Please select an option");
+                if(s.equals("1")) {
+                    //log
+                }
+                else if(s.equals("2")){
+                    //inverse log
+                }
+                else if(s.equals("3")){
+                    //ln
+                }
+                else if(s.equals("4")){
+                    //e^x
+                }
+                else if (s.equals("a")) {
+                    //Clear Error
+                    s = "";
+                }
+                else if (s.equals("b")) {
+                    //Clear Display
+                    s = "";
+                }
+                else if (s.equals("c")) {
+                    //Back
+                    SciMenuHome();
+                }
+                else if (s.equals("d")) {
+                    //Main Menu
+                    Greeting();
+                }
+                else if (s.equals("e")) {
+                    //Exit
+                    ExitCalc();
+                }
+                else {
+                    System.out.println("Please enter a valid response");
+                }
+            }
+//End Log Function
+//Start Factorial Function
+            else if(s.equals("6")) {
+                //Factorial Functions
+            }
+//End Factorial Function
+            else if (s.equals("a")) {
+                //Clear Error
+                s = "";
+            }
+            else if (s.equals("b")) {
+                //Clear Display
+                s = "";
+            }
+            else if (s.equals("c")) {
+                //Back
+                Greeting();
+            }
+            else if (s.equals("d")) {
+                //Main Menu
+                Greeting();
+            }
+            else if (s.equals("e")) {
+                //Exit
+                ExitCalc();
+            }
+            else {
+                System.out.println("Please enter a valid response");
             }
         }
 //End Scientific
@@ -201,7 +410,7 @@ public class MainApplication {
         else {
             System.out.println("Please enter a valid response");
         }
-        BasicMenu();
+        Greeting();
         s = Console.getStringInput("Please select an option");
     }
 
