b5bb7bc618e2c38068f1880e36dcee24da5cff8d
Moe Aydin <maydin@zipcodes-MacBook-Pro-7.local>
2020-02-07 21:41:17 -0500

    added all core features

--- src/main/java/com/zipcodewilmington/scientificcalculator/Operations.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Operations.java
@@ -2,8 +2,56 @@ package com.zipcodewilmington.scientificcalculator;
 
 public class Operations {
 
+    // CORE FEATURES
+    //====================================================
+
+    // Addition
     public static long add(long a, long b){
         return a + b;
     }
+
+    //Subtraction
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    // Multiplication
+    public double multiply (double num1, double num2) {
+        return num1 * num2;
+    }
+
+    // Division
+    public double divide (double num1, double num2) {
+        return num1 / num2;
+    }
+
+    // Number Squared
+    public double square (double num1) {
+        return num1 * num1;
+    }
+
+    // Square Root of Number
+    public double squareRoot (double num1) {
+        return Math.sqrt(num1);
+    }
+
+    // Exponent
+    public double exponent (double num1, double num2) {
+        return Math.pow (num1, num2);
+    }
+
+    // Inverse
+    public double inverse (double num1) {
+        return (1 / num1);
+    }
+
+    // Switch from Positive to Negative
+    public double switchSign (double num1) {
+        return (-1 * num1);
+    }
+
+    // Display Error if Divide by 0
+
+    // Clear Error Before New Operation
 
 }
