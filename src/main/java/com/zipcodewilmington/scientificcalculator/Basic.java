d74a46ba94be70541595c518f4ae165b2edd1b7c
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-08 10:48:49 -0500

    completed Basic methods.

--- src/main/java/com/zipcodewilmington/scientificcalculator/Basic.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Basic.java
@@ -1,33 +1,50 @@
 public class Basic{
-    public float add(){
+    public float add(float a, float b){
+        float sum = a+b;
         return sum;
     }
-    public float subtract(){
+    public float subtract(float a, float b){
+        float difference = a-b;
         return difference;
 
     }
-    public float multiply(){
+    public float multiply(float a, float b){
+        float product = a*b;
         return product;
     }
 
-    public float divide(){
+    public float divide(float a, float b){
+        float quotient = a/b;
         return quotient;
     }
 
-    public float square(){
+    public float square(float a){
+        float squareResult = (float) Math.pow(a,2);
         return squareResult;
 
     }
-    public float squareRoot(){
+    public float squareRoot(float a){
+        float squareRootResult = (float) Math.sqrt(a);
         return squareRootResult;
     }
-    public float exponent(){
+    public float exponent(float a, float b){
+        float exponentResult = (float) Math.pow(a,b);
         return exponentResult;
     }
-    public float inverse(){
+    public float inverse(float a){
+        float inverseResult = 1/a;
         return inverseResult;
     }
-    public float signChange(){
+    public float signChange(float a){
+        float opposite = 0;
+        if (a > 0){
+            opposite = a - (2 * Math.abs(a));
+        } else if(a < 0){
+            opposite = a + (2 * Math.abs(b));
+        } else {
+            opposite = 0;
+        }
+
         return opposite;
     }
 }
