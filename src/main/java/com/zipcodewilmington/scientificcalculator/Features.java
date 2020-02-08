880bd853ee25e1af2096b77ce581e48654ee4acb
Chris Farmer <cfarmer@zipcodes-MacBook-Pro.local>
2020-02-08 12:56:00 -0500

    added base features

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Features.java
@@ -0,0 +1,76 @@
+package com.zipcodewilmington.scientificcalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+public class Features{
+
+    public Features() {
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double substract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public double square(double a) {
+        return a * a;
+    }
+
+    public double root(double a) {
+        return sqrt(a);
+    }
+
+    public double exponent(double a, double b) {
+        return pow(a, b);
+    }
+
+    public double inverse(double a) {
+        return 1 / a;
+    }
+
+    public double invert(double a) {
+        return a * -1;
+    }
+
+    public double clear() {
+        return 0;
+    }
+
+    public void switchDisplayMode() {
+        if (displayMode.equals("binary")) {
+            displayMode = "octal";
+        } else if (displayMode.equals("octal")) {
+            displayMode = "decimal";
+        } else if (displayMode.equals("decimal")) {
+            displayMode = "hexadecimal";
+        } else {
+            displayMode = "binary";
+        }
+    }
+
+    public void switchDisplayMode(String mode) {
+        displayMode = mode;
+    }
+
+    public void storeMemory() {
+        List<Double> memory = new ArrayList<Double>;
+    }
+
+
+}
+
