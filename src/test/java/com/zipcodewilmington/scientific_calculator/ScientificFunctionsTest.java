9e3c803f4d2ef9a64f9b5bc812b2313438aa58dc
Jeremy McCray <jmccray@zipcodes-MacBook-Pro.local>
2020-02-08 15:19:27 -0500

    added a bunch of tests and went to eat

--- src/test/java/com/zipcodewilmington/scientific_calculator/ScientificFunctionsTest.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/ScientificFunctionsTest.java
@@ -97,6 +97,68 @@ public class ScientificFunctionsTest {
         Double actual = ScientificFunctions.squareRoot(10. );
         Assert.assertEquals( assumed, actual);
     }
+
+    @Test
+    public void SquareReturnPositiveWholeNumber(){
+        Double assumed = 4.;
+        Double actual = ScientificFunctions.square(2. );
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void SquareReturnNegativeWholeNumber(){
+        Double assumed = -25.;
+        Double actual = ScientificFunctions.square(-5. );
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void SquareReturnPositiveDecimal(){
+        Double assumed = 30.25;
+        Double actual = ScientificFunctions.square(5.5 );
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void SquareReturnNegativeDecimal(){
+        Double assumed = -30.25;
+        Double actual = ScientificFunctions.square(-5.5 );
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void ChangeSignFromPositive(){
+        Double assumed = -5.;
+        Double actual = ScientificFunctions.changeSign(5.);
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void ChangeSignFromNegative(){
+        Double assumed = 5.;
+        Double actual = ScientificFunctions.changeSign(-5.);
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void SinePositiveNumber(){
+        Double assumed = -0.5440211108893698;
+        Double actual = ScientificFunctions.sine(10.);
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void SineNegativeNumber(){
+        Double assumed = 0.5440211108893698;
+        Double actual = ScientificFunctions.sine(-10.);
+        Assert.assertEquals( assumed, actual);
+    }
+    @Test
+    public void SineZeroNumber(){
+        Double assumed = 0.;
+        Double actual = ScientificFunctions.sine(0.);
+        Assert.assertEquals( assumed, actual);
+    }
 /*
     @Test
     public void squareRootReturnZero(){
@@ -113,5 +175,82 @@ public class ScientificFunctionsTest {
         Assert.assertEquals( assumed, actual);
     }*/
 
+    @Test
+    public void CosineNegativeNumber(){
+        Double assumed = -0.8390715290764524;
+        Double actual = ScientificFunctions.cosine(-10.);
+        Assert.assertEquals( assumed, actual);
+    }
+    @Test
+    public void CosinePositiveNumber(){
+        Double assumed = -0.8390715290764524;
+        Double actual = ScientificFunctions.cosine(10.);
+        Assert.assertEquals( assumed, actual);
+    }
 
+    @Test
+    public void CosineZeroNumber(){
+        Double assumed = 1.;
+        Double actual = ScientificFunctions.cosine(0.);
+        Assert.assertEquals( assumed, actual);
+    }
+    @Test
+    public void TangentZeroNumber(){
+        Double assumed = 0.;
+        Double actual = ScientificFunctions.tangent(0.);
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void TangentPositiveNumber(){
+        Double assumed = 0.6483608274590866;
+        Double actual = ScientificFunctions.tangent(10.);
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void TangentNegativeNumber(){
+        Double assumed = -0.6483608274590866;
+        Double actual = ScientificFunctions.tangent(-10.);
+        Assert.assertEquals( assumed, actual);
+    }
+    @Test
+    public void ExponentPositiveNumbers(){
+        Double assumed = 4.;
+        Double actual = ScientificFunctions.exponent(2.,2.);
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void ExponentNegativeNumbers(){
+        Double assumed = 4.;
+        Double actual = ScientificFunctions.exponent(-2.,2.);
+        Assert.assertEquals( assumed, actual);
+    }
+    @Test
+    public void ExponentNegativeExponentPositiveNumber(){
+        Double assumed = .25;
+        Double actual = ScientificFunctions.exponent(2.,-2.);
+        Assert.assertEquals( assumed, actual);
+    }
+    @Test
+    public void ExponentNegativeExponentNegativeNumber(){
+        Double assumed = .25;
+        Double actual = ScientificFunctions.exponent(-2.,-2.);
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void ExponentZeroPositiveNumber(){
+        Double assumed = 1.;
+        Double actual = ScientificFunctions.exponent(2.,.0);
+        Assert.assertEquals( assumed, actual);
+    }
+
+    @Test
+    public void ExponentZeroZeroNumber(){
+        Double assumed = 1.;
+        Double actual = ScientificFunctions.exponent(0.,.0);
+        Assert.assertEquals( assumed, actual);
+    }
 }
