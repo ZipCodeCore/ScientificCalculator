35bef57b10c9438870f70363dfbf7b169378fd88
Adam Bennett <nyoxidetwitter@gmail.com>
2020-02-07 20:24:24 -0500

    made some calculator

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator/Calculator.java
@@ -1,6 +1,282 @@
 package com.zipcodewilmington.scientificcalculator.Calculator;
 
+import com.zipcodewilmington.scientificcalculator.Utilities.Util;
+
 public class Calculator 
 {
+	private float trueVal;
+	private float storedVal;
+	private boolean allowNegative;
+	private boolean isError;
+	private boolean displayingStoredVal;
+	private DisplayMode numMode;
+	private TrigDisplayMode trigMode;
+	private String display;
+	
+	public enum DisplayMode {
+		BINARY,
+		OCTAL,
+		DECIMAL,
+		HEXADECIMAL
+	}
+	
+	public enum TrigDisplayMode {
+		DEGREES,
+		RADIANS
+	}
+	
+	public Calculator() {
+		this(0, 0, true, DisplayMode.DECIMAL, TrigDisplayMode.DEGREES);
+	}
+	
+	public Calculator(int displayVal, int storedVal, boolean allowNegatives, DisplayMode numMode, TrigDisplayMode trigMode) {
+		this.trueVal = displayVal;
+		this.storedVal = storedVal;
+		this.allowNegative = allowNegatives;
+		this.isError = false;
+		this.displayingStoredVal = false;
+		this.display = "";
+		this.trigMode = trigMode;
+		this.numMode = numMode;
+		updateDisplay();
+	}
 
+	@SuppressWarnings("incomplete-switch")
+	public void updateDisplay() {
+		if (this.isError) {
+			this.display = "Err";
+			return;
+		}
+		
+		float numToDisplay;
+		if (this.displayingStoredVal) {
+			numToDisplay = this.storedVal;
+		}
+		else {
+			numToDisplay = this.trueVal;
+		}
+		
+		switch (this.numMode) {
+			case BINARY:
+				this.display = "" + Integer.toBinaryString((int)numToDisplay);
+				return;
+			case HEXADECIMAL:
+				this.display = "" + Float.toHexString(numToDisplay);
+				return;
+			case OCTAL:
+				this.display = "" + Integer.toOctalString((int)numToDisplay);
+				return;
+		}
+		
+		if (!this.allowNegative && numToDisplay < 0) {
+			numToDisplay = 0;
+		}
+		
+		this.display = "" + numToDisplay;
+	}
+	
+	
+	public void clearDisplay() {
+		this.trueVal = 0;
+		this.isError = false;
+		this.allowNegative = true;
+		updateDisplay();
+	}
+	
+	public void clearMemory() {
+		this.storedVal = 0;
+	}
+
+	
+	// MATH ////////////////////////////////////////////////////////////////////////////////////////////////
+	public void add(float val) {
+		this.trueVal += val;
+		updateDisplay();
+	}
+	
+	public void subtract(float val) {
+		this.trueVal -= val;
+		updateDisplay();
+	}
+	
+	public void mult(float val) {
+		this.trueVal *= val;
+		updateDisplay();
+	}
+	
+	public void div(float num) {
+		this.trueVal /= num;
+		updateDisplay();
+	}
+	
+	public void sqRt() {
+		this.trueVal = Util.squareRoot(this.trueVal);
+		updateDisplay();
+	}
+	
+	public void square() {
+		this.trueVal = Util.square(this.trueVal);
+		updateDisplay();
+	}
+
+	public void pow(float exponent) {
+		this.trueVal = (float) Math.pow(this.trueVal, exponent);
+		updateDisplay();
+	}
+	
+	public void inverse() {
+		this.trueVal = 1.0f / this.trueVal;
+		updateDisplay();
+	}
+	
+	public void flipSign() {
+		this.trueVal = -this.trueVal;
+		updateDisplay();
+	}
+	
+	public void sine() {
+		this.trueVal = Util.sine(this.trueVal);
+		if (this.trigMode == TrigDisplayMode.RADIANS) {
+			this.trueVal = Util.toRadians(this.trueVal);
+		}
+	}
+	
+	public void cosine() {
+		this.trueVal = Util.cosine(this.trueVal);
+		if (this.trigMode == TrigDisplayMode.RADIANS) {
+			this.trueVal = Util.toRadians(this.trueVal);
+		}
+	}
+	
+	public void tangent() {
+		this.trueVal = Util.tangent(this.trueVal);
+		if (this.trigMode == TrigDisplayMode.RADIANS) {
+			this.trueVal = Util.toRadians(this.trueVal);
+		}
+	}
+	
+	public void invSine() {
+		this.trueVal = Util.invSine(this.trueVal);
+		if (this.trigMode == TrigDisplayMode.RADIANS) {
+			this.trueVal = Util.toRadians(this.trueVal);
+		}
+	}
+	
+	public void invCosine() {
+		this.trueVal = Util.invCosine(this.trueVal);
+		if (this.trigMode == TrigDisplayMode.RADIANS) {
+			this.trueVal = Util.toRadians(this.trueVal);
+		}
+	}
+	
+	public void invTangent() {
+		this.trueVal = Util.invTangent(this.trueVal);
+		if (this.trigMode == TrigDisplayMode.RADIANS) {
+			this.trueVal = Util.toRadians(this.trueVal);
+		}
+	}
+	// END MATH ///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+	// GETTERS ////////////////////////////////////////////////////////////////////////////////////////////////
+	public String getDisplay() {
+		return display;
+	}
+	
+	public float getDisplayVal() {
+		if (this.trueVal > 0.0f || this.allowNegative) {
+			return this.trueVal;
+		}
+		else {
+			return 0.0f;
+		}
+	}
+	
+	public float getStoredVal() {
+		return this.storedVal;
+	}
+	
+	public boolean isInErrorMode() {
+		return this.isError;
+	}
+	
+	public boolean allowingNegative() {
+		return this.allowNegative;
+	}
+	
+	public DisplayMode getNumMode() {
+		return this.numMode;
+	}
+	
+	public String getNumModeStr() {
+		switch (this.numMode) {
+			case BINARY:
+				return "Binary";
+			case DECIMAL:
+				return "Decimal";
+			case HEXADECIMAL:
+				return "Hexadecimal";
+			case OCTAL:
+				return "Octal";
+			default:
+				return "Uh.OH.";
+		}
+	}
+	
+	public TrigDisplayMode getTrigMode() {
+		return this.trigMode;
+	}
+	
+	public String getTrigModeStr() {
+		switch (this.trigMode) {
+			case DEGREES:
+				return "Degrees";
+			case RADIANS:
+				return "Radians";
+			default:
+				return "Uh.OH.";		
+		}
+	}
+	// END GETTERS ///////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+	// SETTERS ///////////////////////////////////////////////////////////////////////////////////////////////
+	public void setDisplayVal(int val) {
+		this.trueVal = val;
+		updateDisplay();
+	}
+	
+	public void setAllowNegative(boolean allow) {
+		this.allowNegative = allow;
+		updateDisplay();
+	}
+	
+	public void setDisplay(String newDisp) {
+		this.display = newDisp;
+		updateDisplay();
+	}
+	
+	public void setError(boolean isError) {
+		this.isError = isError;
+		updateDisplay();
+	}
+	
+	public void setStoredVal(int newStoredVal) {
+		this.storedVal = newStoredVal;
+	}
+	
+	public void incStoredVal(int incrementAmt) {
+		this.storedVal += incrementAmt;
+	}
+	
+	public void setDisplayMode(DisplayMode newMode) {
+		this.numMode = newMode;
+	}
+	
+	public void setTrigMode(TrigDisplayMode newMode) {
+		this.trigMode = newMode;
+	}
+	// END SETTERS /////////////////////////////////////////////////////////////////////////////////////////
+	
+	
 }
--- src/main/java/com/zipcodewilmington/scientificcalculator/Utilities/Util.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Utilities/Util.java
@@ -2,5 +2,96 @@ package com.zipcodewilmington.scientificcalculator.Utilities;
 
 public class Util 
 {
+	public static int add(int num1, int num2) {
+		return num1 + num2;
+	}
 	
+	public static int sub(int left, int right) {
+		return left - right;
+	}
+	
+	public static int mult(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public static float div(float numerator, float denominator) {
+		return numerator / denominator;
+	}
+	
+	public static int div(int numerator, int denominator) {
+		return (int)div((float)numerator, (float)denominator);
+	}
+	
+	public static float square(float num) {
+		return num * num;
+	}
+	
+	public static float squareRoot(float num) {
+		return (float) Math.sqrt(num);
+	}
+	
+	public static int squareRoot(int num) {
+		return (int)squareRoot((float)num);
+	}
+	
+	public static double varExp(double base, double exp) {
+		return Math.pow(base, exp);
+	}
+	
+	public static int varExp(int base, int exp) {
+		return (int)Math.pow((double)base, (double)exp);
+	}
+	
+	public static int inverse(int toInverse) {
+		return div(1, toInverse);
+	}
+	
+	public static int flipSign(int num) {
+		return -num;
+	}
+	
+	public static String returnErr() {
+		return "Err";
+	}
+	
+	public static float sine(float num) {
+		return (float) Math.sin(num);
+	}
+	
+	public static float cosine(float num) {
+		return (float) Math.cos(num);
+	}
+	
+	public static float tangent(float num) {
+		return (float) Math.tan(num);
+	}
+	
+	public static float invSine(float num) {
+		return (float) Math.asin(num);
+	}
+	
+	public static float invCosine(float num) {
+		return (float) Math.acos(num);
+	}
+	
+	public static float invTangent(float num) {
+		return (float) Math.atan(num);
+	}
+	
+	public static float toRadians(float num) {
+		return (float) Math.toRadians(num);
+	}
+	
+	public static double log(double num) {
+		return Math.log(num);
+	}
+	
+	public static double inverseNaturalLog(double num) {
+		return Math.expm1(num) + 1;
+	}
+	
+	public static int factorial(int n) 
+    {   	
+    	return (n == 1 || n == 0) ? 1 : n * factorial(n - 1);
+    } 
 }
