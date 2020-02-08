bfd2fcd53010de73796dd67e3da7f0b172b1bbeb
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-07 19:27:19 -0500

    first test commit

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -5,14 +5,16 @@ package com.zipcodewilmington.scientificcalculator;
  */
 public class MainApplication {
     public static void main(String[] args) {
-        Console.println("Welcome to my calculator!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
-
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
-        //i did thisfdsgsgfs
+//        Console.println("Welcome to my calculator!");
+//        String s = Console.getStringInput("Enter a string");
+//        Integer i = Console.getIntegerInput("Enter an integer");
+//        Double d = Console.getDoubleInput("Enter a double.");
+//
+//        Console.println("The user input %s as a string", s);
+//        Console.println("The user input %s as a integer", i);
+//        Console.println("The user input %s as a d", d);
+        System.out.printf("%-10s %-10s %-10s", "1 - Add |", "2 - Subtract |" , "3 - Multiply \n");
+        System.out.printf("%-10s %-10s %-10s", "4 - Divide |", "5 - Square |" , "6 - Square Root \n");
+        System.out.printf("%-10s %-10s %-10s", "7 - Exponent |", "8 - Inverse |" , "9 - Inverse Sign");
     }
 }
