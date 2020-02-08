5011f42036475aea1aeb40d5d87cd7af770f5974
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-02-08 11:56:10 -0500

    added/updated functions

--- src/main/java/com/zipcodewilmington/scientificcalculator/ScientificFunctions.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ScientificFunctions.java
@@ -26,8 +26,13 @@ public class ScientificFunctions {
     }
 
     public Double Divide(Double a, Double b) {
-        Double sum = a / b;
-        return sum;
+        if (b == 0){
+            // tell user division by 0 is not allowed and to re enter input
+        }
+        else {
+            Double sum = a / b;
+            return sum;
+        }
     }
 
     public Double SquareRoot(Double a) {
@@ -35,68 +40,96 @@ public class ScientificFunctions {
         return sum;
     }
 
-    public Double Square(Double a, Double b) {
-        Double sum = a - b;
+    public Double Square(Double a) {
+        Double sum = a*a;
         return sum;
     }
-
-    public Double Inverse() {
-        return null;
+    // Add all inverses?  Multiplicative/additive/
+    public Double Inverse(Double a) {
+        Double sum = (1/a);
+        return sum;
     }
-
+    // reset memory to 0?
     public void Clear() {
 
     }
 
+    public Double exponent(Double a, Double e){
+        Double sum = Math.pow(a, e);
+        return sum;
+    }
+
+    public Double changeSign(Double a){
+        if(a < 0){
+            Double value = Math.abs(a);
+        }
+        else{
+            value = value - (value*2);
+        }
+        return value;
+    }
+
+
 
     // returns sine
-    public double sine(double input){
-        double number = input;
+    public Double sine(Double input){
+        Double number = input;
         return Math.sin(number);
     }
     // returns cosine
-    public double cosine(double input){
-        double number = input;
+    public Double cosine(Double input){
+        Double number = input;
         return Math.cos(number);
     }
     // returns tangent
-    public double tangent(double input){
-        double number = input;
+    public Double tangent(Double input){
+        Double number = input;
         return Math.tan(number);
     }
     // inverse sine
-    public double inverseSine(double input){
-        double number = input;
+    public Double inverseSine(Double input){
+        Double number = input;
         return Math.asin(number);
     }
     // inverse cosine
-    public double inverseCosine(double input){
-        double number = input;
+    public Double inverseCosine(Double input){
+        Double number = input;
         return Math.acos(number);
     }
     // inverse tangent
-    public double inverseTangent(double input){
-        double number = input;
+    public Double inverseTangent(Double input){
+        Double number = input;
         return Math.atan(number);
     }
 
     // to radians
 
-    public double radianCalc(double input){
-        double number = input;
+    public Double radian(Double input){
+        Double number = input;
         return Math.toRadians(number);
     }
 
     // to degrees
-    public double degreesCalc(double input){
-        double number = input;
+    public Double degree(Double input){
+        Double number = input;
         return Math.toDegrees(number);
     }
 
-    // calc log
-    public double calcLog(double input){
-        double number = input;
+    // calc natural log
+    public Double naturalLog(Double input){
+        Double number = input;
         return Math.log(number);
+    }
+
+    // calc base log
+    public Double baseLog(Double input){
+        Double number = input;
+        return Math.log10(number);
+    }
+
+    public Double inverseLog(Double input){
+        Double number = Math.pow(10, input);
+        return number;
     }
 
 }
