1c0ca77650fda047b1fd7996af06f6f6a932bddf
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-08 12:26:24 -0500

    dfsa

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -13,14 +13,29 @@ public class MainApplication {
         String s = Console.getStringInput("1 - Basic | 2 - Scientific | 3 - Exit");
         if(s.equals("1")) {
             BasicMenu();
-            s = Console.getStringInput("Please select an option");
-            switch (s) {
-                case "1":
 
-                    s = Console.getStringInput(" Enter in following format \n a a a a \n Press enter when done");
-                    String sumString = s;
-                    System.out.println((Addition(sumString));
+            s = Console.getStringInput("Please select an option");
+            if(s.equals("1")){
+                Integer curInt = 0;
+                Integer pastInt = 0;
+                curInt = Console.getIntInput("Please enter a number\nEnter x when done");
+                while(curInt != -0){
+                    System.out.println(curInt);
+                    Addition(pastInt, curInt);
+                    pastInt = curInt;
+
+                }
             }
+//            else if(s.equals("2")) {
+//                s = Console.getStringInput(" Enter in following format \n a a a a \n Press enter when done");
+//                String resString = s;
+//                System.out.println((Addition(resString));
+//            }
+//            else if(s.equals("3")) {
+//                s = Console.getStringInput(" Enter in following format \n a a a a \n Press enter when done");
+//                String resString = s;
+//                System.out.println((Addition(resString)));
+//            }
         }
         else if(s.equals("2")) {
             SciMenu();
@@ -35,4 +50,5 @@ public class MainApplication {
         }
 
 
+    }
 }
