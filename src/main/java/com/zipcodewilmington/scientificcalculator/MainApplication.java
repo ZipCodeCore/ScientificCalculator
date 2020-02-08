1744e1a449b022ff1e3793743eefb3aef7c43074
Giles Bradford <gbradford@zipcodes-MacBook-Pro-4.local>
2020-02-07 21:41:00 -0500

    adding code for the night

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -10,12 +10,24 @@ public class MainApplication {
 
         Console.println("Welcome to my calculator!");
         Double number1 = Console.getDoubleInput("What number do you want to start with? :  ");
-            coreFunctions.IsANumber(number1);
+            //coreFunctions.IsANumber(number1);
 
-        String operator = Console.getStringInput("Please enter a second number : ");
-
+        String operator = Console.getStringInput("Please enter your desired operator : ");
         Double number2 = Console.getDoubleInput("Please enter a second number : ");
 
+        if (operator.equals( "+")) {
+            System.out.println(coreFunctions.Add(number1, number2));
+        } else if (operator == "-") {
+            System.out.println(coreFunctions.Subtract(number1, number2));
+        } else if (operator == "*") {
+            System.out.println(coreFunctions.Multiply(number1, number2));
+        } else if (operator == "/") {
+            System.out.println(coreFunctions.Divide(number1, number2));
+        } else if (operator == "sqrt") {
+            System.out.println(coreFunctions.SquareRoot(number1));
+        } else {
+
+        }
 
         Console.println("The user input %s as the first number.", number1);
         Console.println("The user input %s as the operator", operator);
