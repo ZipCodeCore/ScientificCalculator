3b1fa098fc248d82c7e1b60aa8d91cb0b879c66e
vle <obentoka@gmail.com>
2020-02-07 19:05:30 -0500

    next code

--- src/test/java/com/zipcodewilmington/scientific_calculator/OperationsTest.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/OperationsTest.java
@@ -1,13 +1,18 @@
 package com.zipcodewilmington.scientific_calculator;
 
+import com.zipcodewilmington.scientificcalculator.Operations;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class OperationsTest {
 
     @Test
     public void addTest(){
-
+        Operations a = new Operations();
         long expected = 1000000000;
-        long actual = add(50000, 50000);
+        long actual = Operations.add(500000000, 500000000);
+        assertEquals(expected, actual);
+
     }
 }
