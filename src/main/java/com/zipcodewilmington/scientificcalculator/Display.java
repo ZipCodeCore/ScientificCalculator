369df63965133610ca158882f17e9a20570ce2bc
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-08 18:03:35 -0500

    Please don't kill me

--- src/main/java/com/zipcodewilmington/scientificcalculator/Display.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Display.java
@@ -1,32 +1,53 @@
-/*
-
 package com.zipcodewilmington.scientificcalculator;
 
 public class Display {
-    Double currentValue;
-    String currentOperation;
-    Boolean error;
-    enum Modes { BINARY, OCTAL, DEC, HEX }
-    Modes mode;
+    private Double currentValue;
+    private String currentOperation;
+    private Boolean error;
+    private enum Modes { BINARY, OCTAL, DEC, HEX }
+    private Modes mode;
 
-    public void Display() {
-        if(error) {
-            // Print "ERR"
-        } else {
-            //if(mode)
-        }
+    public Display()
+    {
+        currentValue = 0.0;
+        mode = Modes.DEC;
+        error = false;
+        // currentOperation
     }
 
-    public void update() {
+    public void update()
+    {
+        String output;
 
+        if(error) {
+            output = "ERR";
+        }
+        else if(mode == Modes.BINARY)
+        {
+
+        }
+        else if(mode == Modes.OCTAL)
+        {
+
+        }
+        else if(mode == Modes.DEC)
+        {
+            output = Double.toString(currentValue);
+        }
+        else if(mode == Modes.HEX)
+        {
+
+        }
+
+//        Console.println(output);
     }
 
     public void clear() {
-
+        currentValue = 0.0;
+        update();
     }
 
-    public void invSign() {
-        return currentValue * -1;
+    public Double invSign() {
+        return currentValue * -1.0;
     }
 }
-*/
---
+++ src/test/java/com/zipcodewilmington/scientificcalculator/DisplayTest.java
@@ -0,0 +1,23 @@
+package com.zipcodewilmington.scientificcalculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DisplayTest {
+
+    @Test
+    public void update() {
+        String expected = "0";
+        Display TestDisplay = new Display();
+      //  String actual = String.toString(TestDisplay.update());
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void invSign() {
+    }
+}
