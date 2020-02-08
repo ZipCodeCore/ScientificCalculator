908610105910f4f32d842e48948fc7a5955a4a0d
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-07 19:13:22 -0500

    added outline of basic functions and some sci functions, test push here.

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Basic.java
@@ -0,0 +1,33 @@
+public class Basic{
+    public float add(){
+        return sum;
+    }
+    public float subtract(){
+        return difference;
+
+    }
+    public float multiply(){
+        return product;
+    }
+
+    public float divide(){
+        return quotient;
+    }
+
+    public float square(){
+        return squareResult;
+
+    }
+    public float squareRoot(){
+        return squareRootResult;
+    }
+    public float exponent(){
+        return exponentResult;
+    }
+    public float inverse(){
+        return inverseResult;
+    }
+    public float signChange(){
+        return opposite;
+    }
+}
---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Sci.java
@@ -0,0 +1,18 @@
+public class Sci {
+    public String switchDisplayMode(){
+        return displayMode;
+    }
+    public class Memory {
+        public void memPlus(){
+
+        }
+        public void memCLear(){
+
+
+        }
+        public float recall(){
+            return memNum;
+        }
+
+    }
+}
