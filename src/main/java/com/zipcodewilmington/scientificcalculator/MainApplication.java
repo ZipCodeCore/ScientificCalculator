b662544b449179f28d178787d609bb23fbd58e22
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-08 11:15:58 -0500

    Small comment edit

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington.scientificcalculator;
 
 /**
  * Created by leon on 2/9/18.
+ * James Wilkinson
  */
 public class MainApplication {
     public static void main(String[] args) {
