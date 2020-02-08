8737a56f2ccffbdfecc88a5d0a3187aca24d0fc8
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-08 11:44:05 -0500

    completed Sci methods

--- src/main/java/com/zipcodewilmington/scientificcalculator/Sci.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Sci.java
@@ -16,22 +16,28 @@ public class Sci {
         public float recall(){
             return memNum;
         }
-    public float sine(){
+    public float sine(float a){
+        float sin = (float) Math.sin(a);
         return sin;
     }
-    public float cosine(){
+    public float cosine(float a){
+        float cos = (float) Math.cos(a);
         return cos;
     }
-    public float tangent(){
+    public float tangent(float a){
+        float tan = (float) Math.tan(a);
         return tan;
     }
-    public float inverseSine(){
+    public float inverseSine(float a){
+        float invSin = (float) Math.asin(a);
         return invSin;
     }
-    public float inverseCosine(){
+    public float inverseCosine(float a){
+        float invCos = (float) Math.acos(a);
         return invCos;
     }
-    public float inverseTangent(){
+    public float inverseTangent(float a){
+        float invTan = (float) Math.atan(a);
         return invTan;
     }
     public String switchUnitsModeRotate(){
@@ -41,19 +47,27 @@ public class Sci {
     public String switchUnitsModeString(String unitsModeInput){
         return unitsModeInput;
     }
-    public float logarithm(){
+    public float logarithm(float a){
+        float log = (float) Math.log10(a);
         return log;
     }
-    public float inverseLogarithm(){
+    public float inverseLogarithm(float a){
+        float invLog = (float) 1 / Math.log10(a);
         return invLog;
     }
-    public float naturalLogarithm(){
+    public float naturalLogarithm(float a){
+        float naturalLogarithm = (float) Math.log(a);
         return natLog;
     }
-    public float inverseNaturalLogarithm(){
+    public float inverseNaturalLogarithm(float a){
+        float invNatLog = (float) (1/Math.log(a));
         return invNatLog;
     }
-    public Integer factorial(){
+    public Integer factorial(float a){
+        float fact = 0;
+        for (int i = 0; i< a;a++){
+            fact *= a-1;
+        }
         return fact;
     }
 }
