a7762d94c74f41c5728f246c666407996901eb57
Chris Farmer <cfarmer@zipcodes-MacBook-Pro.local>
2020-02-08 12:59:14 -0500

    added base features

--- src/main/java/com/zipcodewilmington/scientificcalculator/Features.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Features.java
@@ -50,27 +50,5 @@ public class Features{
     public double clear() {
         return 0;
     }
-
-    public void switchDisplayMode() {
-        if (displayMode.equals("binary")) {
-            displayMode = "octal";
-        } else if (displayMode.equals("octal")) {
-            displayMode = "decimal";
-        } else if (displayMode.equals("decimal")) {
-            displayMode = "hexadecimal";
-        } else {
-            displayMode = "binary";
-        }
-    }
-
-    public void switchDisplayMode(String mode) {
-        displayMode = mode;
-    }
-
-    public void storeMemory() {
-        List<Double> memory = new ArrayList<Double>;
-    }
-
-
 }
 
--- src/main/java/com/zipcodewilmington/scientificcalculator/SciFeatures.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/SciFeatures.java
@@ -1,5 +1,31 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SciFeatures {
+
+    String displayMode;
+    
+    public void switchDisplayMode() {
+        if (displayMode.equals("binary")) {
+            displayMode = "octal";
+        } else if (displayMode.equals("octal")) {
+            displayMode = "decimal";
+        } else if (displayMode.equals("decimal")) {
+            displayMode = "hexadecimal";
+        } else {
+            displayMode = "binary";
+        }
+    }
+
+    public void switchDisplayMode(String mode) {
+        displayMode = mode;
+    }
+
+    public void storeMemory() {
+        List<Double> memory = new ArrayList<Double>;
+    }
+
 
 }
