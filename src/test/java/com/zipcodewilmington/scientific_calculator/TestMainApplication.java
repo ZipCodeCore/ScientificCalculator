3efba81e3eb7ee3af179dddacae4adf78aba995e
Sandy Setiawan <ssetiawan@zipcodes-MacBook-Pro.local>
2020-02-09 09:20:04 -0500

    TestMainApp

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -3,5 +3,10 @@ package com.zipcodewilmington.scientific_calculator;
 /**
  * Created by leon on 2/9/18.
  */
-public class TestMainApplication {
+public class MainApplication {
+    MainApplication test = new MainApplication();
+
+    @Test
+public class addTest(){
+    Assert.assertEquals(expected: 10, test.add(a: 5, b: 5)
 }
