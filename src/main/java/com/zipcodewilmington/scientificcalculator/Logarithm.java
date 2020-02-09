139b464a1901d02fec1e601d6b003c29a78ed2f9
Sandeep Naraya <snaraya@zipcodes-MacBook-Pro-9.local>
2020-02-09 12:56:32 -0500

    Minor changes

--- src/main/java/com/zipcodewilmington/scientificcalculator/Logarithm.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Logarithm.java
@@ -10,7 +10,7 @@ public class Logarithm {
 
     }
 
-    public static Double Log(Double x) {
+    public static double Log(Double x) {
 
         if (x<=0)
            return 0.0;
@@ -34,7 +34,7 @@ public class Logarithm {
 
     }
 
-    public static Double InvNLog(Double x) {
+    public static double InvNLog(Double x) {
 
         return (Math.pow(Math.exp(1.0), x));
 
