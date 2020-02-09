a9c39a1fd0333ae6f0801eff8cd4cde15154cc4f
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-08 19:13:02 -0500

    About to pull dev

--- src/main/java/com/zipcodewilmington/scientificcalculator/Display.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Display.java
@@ -15,39 +15,70 @@ public class Display {
         // currentOperation
     }
 
-    public void update()
+    public void setDisplay(Double inputValue)
     {
-        String output;
+        currentValue = inputValue;
+    }
+
+    public Double getDisplay()
+    {
+        return currentValue;
+    }
+
+    public String Update()
+    {
+        String output = "";
 
         if(error) {
             output = "ERR";
         }
         else if(mode == Modes.BINARY)
         {
-
+            output = Integer.toBinaryString(currentValue.intValue());
         }
         else if(mode == Modes.OCTAL)
         {
-
+            output = Integer.toOctalString(currentValue.intValue());
         }
         else if(mode == Modes.DEC)
         {
-            output = Double.toString(currentValue);
+            if(currentValue % 1 == 0) // No decimal
+            {
+                output = Integer.toString(currentValue.intValue());
+            }
+            else
+            {
+                output = Double.toString(currentValue);
+            }
         }
         else if(mode == Modes.HEX)
         {
-
+            output = Integer.toHexString(currentValue.intValue());
         }
 
-//        Console.println(output);
+        Console.println("%s", output);
+        return output;
     }
 
-    public void clear() {
+    public void Clear() {
         currentValue = 0.0;
-        update();
+        Update();
     }
 
-    public Double invSign() {
+    public Double InvSign()
+    {
         return currentValue * -1.0;
+    }
+
+    public Double AbsoluteValue()
+    {
+        if(currentValue < 0)
+        {
+            return currentValue * -1;
+        }
+        else
+        {
+            return currentValue;
+        }
     }
 }
--- src/test/java/com/zipcodewilmington/scientificcalculator/DisplayTest.java
+++ src/test/java/com/zipcodewilmington/scientificcalculator/DisplayTest.java
@@ -7,17 +7,67 @@ import static org.junit.Assert.*;
 public class DisplayTest {
 
     @Test
-    public void update() {
+    public void TestUpdateInitValue()
+    {
         String expected = "0";
         Display TestDisplay = new Display();
-      //  String actual = String.toString(TestDisplay.update());
+        String actual = TestDisplay.Update();
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void clear() {
+    public void TestClear() {
+        Double expected = 0.0;
+        Display TestDisplay = new Display();
+        Double actual;
+        TestDisplay.setDisplay(99.99999);
+        TestDisplay.Clear();
+        actual = TestDisplay.getDisplay();
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void invSign() {
+    public void TestInvSign() {
+        Double expected = -47.6;
+        Display TestDisplay = new Display();
+        TestDisplay.setDisplay(47.6);
+        Double actual = TestDisplay.InvSign();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestAbsoluteValueOfTen()
+    {
+        Double expected = 10.0;
+        Display TestDisplay = new Display();
+        TestDisplay.setDisplay(10.0);
+        Double actual = TestDisplay.AbsoluteValue();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestAbsoluteValueOfNegFourtyFive()
+    {
+        Double expected = 45.0;
+        Display TestDisplay = new Display();
+        TestDisplay.setDisplay(-45.0);
+        Double actual = TestDisplay.AbsoluteValue();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestAbsoluteValueOfZero()
+    {
+        Double expected = 0.0;
+        Display TestDisplay = new Display();
+        TestDisplay.setDisplay(0.0);
+        Double actual = TestDisplay.AbsoluteValue();
+
+        assertEquals(expected, actual);
     }
 }
