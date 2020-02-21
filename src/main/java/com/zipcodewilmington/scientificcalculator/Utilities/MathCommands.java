9a8810442d52af30274b375a1dcad14b1a6f338c
Adam Bennett <nyoxidetwitter@gmail.com>
2020-02-20 22:27:23 -0500

    added brackets to expression code, Dijkstra is clearly a lunatic

--- src/main/java/com/zipcodewilmington/scientificcalculator/Utilities/MathCommands.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Utilities/MathCommands.java
@@ -317,11 +317,11 @@ public class MathCommands
 			else if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/") || s.equals("sqrt")) { ops.push(s); }
 			else if (s.equals(")")) {
 				String op = ops.pop(); double v = vals.pop();
-				if (op.equals("+"))    v = vals.pop() + v;
-				else if (op.equals("-"))    v = vals.pop() - v;
-				else if (op.equals("*"))    v = vals.pop() * v;
-				else if (op.equals("/"))    v = vals.pop() / v;
-				else if (op.equals("sqrt")) v = Math.sqrt(v);
+				if (op.equals("+"))      {    v = vals.pop() + v; }
+				else if (op.equals("-")) {    v = vals.pop() - v; }
+				else if (op.equals("*")) {    v = vals.pop() * v; }
+				else if (op.equals("/")) {    v = vals.pop() / v; }
+				else if (op.equals("sqrt")) { v = Math.sqrt(v);   }
 				vals.push(v);
 			} else vals.push(Double.parseDouble(s));
 		} return vals.pop();
