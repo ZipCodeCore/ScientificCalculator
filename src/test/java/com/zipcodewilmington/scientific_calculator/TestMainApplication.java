0602287c1005e5391a164ab1b52c676fa68aa106
xyuan04 <78838190+xyuan04@users.noreply.github.com>
2021-02-27 16:09:02 -0500

    Created/Tested switchDisplayMode + switchUnitsMode (#1)
    Co-authored-by: xiong <xiong@zipcodes-MBP-8.fios-router.home>

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,53 @@
 package com.zipcodewilmington.scientific_calculator;
+import static org.junit.Assert.assertEquals;
 
+import com.zipcodewilmington.scientificcalculator.MainApplication;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+
+    @Test
+    public void displayTest() {
+        MainApplication displaytester = new MainApplication();
+
+        assertEquals("octal", displaytester.switchDisplayMode());
+        assertEquals("decimal", displaytester.switchDisplayMode());
+        assertEquals("hexadecimal", displaytester.switchDisplayMode());
+    }
+
+    @Test
+    public void modeChange() {
+        MainApplication modeChanger = new MainApplication();
+
+        assertEquals("binary", modeChanger.switchDisplayMode("binary"));
+        assertEquals("hexadecimal", modeChanger.switchDisplayMode("hexadecimal"));
+        assertEquals("Err", modeChanger.switchDisplayMode("hello!"));
+    }
+
+    @Test
+    public void unitChangeButton() {
+        MainApplication unitChanger = new MainApplication();
+
+        assertEquals("radians", unitChanger.switchUnitsMode());
+        assertEquals("degrees", unitChanger.switchUnitsMode());
+        assertEquals("radians", unitChanger.switchUnitsMode());
+    }
+
+    @Test
+    public void unitChange() {
+        MainApplication unitChanger1 = new MainApplication();
+
+        assertEquals("radians", unitChanger1.switchUnitsMode("radians"));
+        assertEquals("Err", unitChanger1.switchUnitsMode("Break me"));
+        assertEquals("degrees", unitChanger1.switchUnitsMode("degrees"));
+    }
+
+
+
+
+
 }
