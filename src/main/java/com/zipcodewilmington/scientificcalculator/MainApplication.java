1a0a26a4e367799675e5013449a0e35e52401fd9
Jorge <jorge@zipcodes-MacBook-Pro-51.local>
2021-02-27 16:32:49 -0500

    Finnished log equations

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,9 +1,12 @@
 package com.zipcodewilmington.scientificcalculator;
-
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
+    public MainApplication(double num1,double num2) {
+
+    }
+
     public static void main(String[] args) {
         Console.println("Welcome to my calculator!");
         String s = Console.getStringInput("Enter a string");
@@ -13,5 +16,39 @@ public class MainApplication {
         Console.println("The user input %s as a string", s);
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a d", d);
+
     }
+
+    /* //Test method used to check that test class was working
+    public static double addNum(double num1, double num2) {
+        double sumOf = num1 + num2;
+        System.out.println(sumOf);
+        return sumOf;
+    }
+
+     */
+
+    //Takes a value for num2 for the log equation. num1 is not used in this case
+    public static double logarithm(double num1, double num2) {
+        double newNum = Math.log10(num2);
+        System.out.println(newNum);
+        return newNum;
+    }
+    public double invLog(double num1, double num2) {
+        double newNum = Math.pow(10,num2);
+        System.out.println(newNum);
+        return newNum;
+    }
+    public double natLog(double num1, double num2) {
+        double newNum = Math.log(num2);
+        System.out.println(newNum);
+        return newNum;
+    }
+    public double invNatLog(double num1, double num2) {
+        double newNum = Math.pow(Math.E,num2);
+        System.out.println(newNum);
+        return newNum;
+    }
+
+
 }
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,42 @@
 package com.zipcodewilmington.scientific_calculator;
 
+import com.zipcodewilmington.scientificcalculator.MainApplication;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by leon on 2/9/18.
  */
+
 public class TestMainApplication {
+
+    /* //Used to make sure test class worked.
+    @Test
+    public void getSum() {
+        MainApplication sum = new MainApplication(2,2);
+        assertEquals(6, sum.addNum(3,3), 0.0000001);
+    }
+     */
+
+    @Test
+    public void getLog10() {
+        MainApplication newLog = new MainApplication(0.0,10.0);
+        assertEquals(1.0, newLog.logarithm((Math.log10(0.0)),10.0), 0.0000001);
+    }
+    @Test
+    public void getInvLog10() {
+        MainApplication newLog = new MainApplication(0.0,5.0);
+        assertEquals(100000.0, newLog.invLog((Math.log10(0.0)),5.0), 0.0000001);
+    }
+    @Test
+    public void getNetLog() {
+        MainApplication newLog = new MainApplication(0.0, 5.0);
+        assertEquals(1.609437912, newLog.natLog((Math.log(0.0)), 5.0), 0.0000001);
+    }
+    @Test
+    public void getInvNetLog() {
+        MainApplication newLog = new MainApplication(0.0, 5.0);
+        assertEquals(148.4131591, newLog.invNatLog((Math.log(0.0)), 5.0), 0.0000001);
+    }
 }
