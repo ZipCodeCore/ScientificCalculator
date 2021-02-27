6c52bbaf13eacceaea9f26e4ac8b4cc75244f845
Jorge <jorge@zipcodes-MacBook-Pro-51.local>
2021-02-27 17:06:54 -0500

    added new test for infinity

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -3,7 +3,7 @@ package com.zipcodewilmington.scientificcalculator;
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
-    public MainApplication(double num1,double num2) {
+    public MainApplication(double num1) {
 
     }
 
@@ -19,33 +19,24 @@ public class MainApplication {
 
     }
 
-    /* //Test method used to check that test class was working
-    public static double addNum(double num1, double num2) {
-        double sumOf = num1 + num2;
-        System.out.println(sumOf);
-        return sumOf;
-    }
-
-     */
-
     //Takes a value for num2 for the log equation. num1 is not used in this case
-    public static double logarithm(double num1, double num2) {
-        double newNum = Math.log10(num2);
+    public static double logarithm(double num1) {
+        double newNum = Math.log10(num1);
         System.out.println(newNum);
         return newNum;
     }
-    public double invLog(double num1, double num2) {
-        double newNum = Math.pow(10,num2);
+    public double invLog(double num1) {
+        double newNum = Math.pow(10,num1);
         System.out.println(newNum);
         return newNum;
     }
-    public double natLog(double num1, double num2) {
-        double newNum = Math.log(num2);
+    public double natLog(double num1) {
+        double newNum = Math.log(num1);
         System.out.println(newNum);
         return newNum;
     }
-    public double invNatLog(double num1, double num2) {
-        double newNum = Math.pow(Math.E,num2);
+    public double invNatLog(double num1) {
+        double newNum = Math.pow(Math.E,num1);
         System.out.println(newNum);
         return newNum;
     }
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -11,38 +11,32 @@ import static org.junit.Assert.*;
 
 public class TestMainApplication {
 
-    /* //Used to make sure test class worked.
-    @Test
-    public void getSum() {
-        MainApplication sum = new MainApplication(2,2);
-        assertEquals(6, sum.addNum(3,3), 0.0000001);
-    }
-     */
-
     @Test //Log base 10 method test
     public void getLog10() {
-        MainApplication newLog = new MainApplication(0.0,10.0);
-        assertEquals(1.0, newLog.logarithm((Math.log10(0.0)),10.0), 0.0000001);
+        MainApplication newLog = new MainApplication(10.0);
+        assertEquals(1.0, newLog.logarithm(10.0), 0.0000001);
     }
     @Test //Log base 10 method Err test
     public void getLog101() {
-        MainApplication newLog = new MainApplication(0.0,10.0);
-        assertEquals(1.0, newLog.logarithm((Math.log10(0.0)),10.0), 0.0000001);
+        MainApplication newLog = new MainApplication(0.0);
+        assertEquals(Double. NEGATIVE_INFINITY, newLog.logarithm(0.0), 0.0000001);
     }
-    @Test //Inverted log 
+
+
+    @Test //Inverted log test
     public void getInvLog10() {
-        MainApplication newLog = new MainApplication(0.0,5.0);
-        assertEquals(100000.0, newLog.invLog((Math.log10(0.0)),5.0), 0.0000001);
+        MainApplication newLog = new MainApplication(5.0);
+        assertEquals(100000.0, newLog.invLog(5.0), 0.0000001);
     }
-    @Test
+    @Test //Natural Log test
     public void getNetLog() {
-        MainApplication newLog = new MainApplication(0.0, 5.0);
-        assertEquals(1.609437912, newLog.natLog((Math.log(0.0)), 5.0), 0.0000001);
+        MainApplication newLog = new MainApplication(5.0);
+        assertEquals(1.609437912, newLog.natLog(5.0), 0.0000001);
     }
-    @Test
+    @Test //Inverted Natural Log test
     public void getInvNetLog() {
-        MainApplication newLog = new MainApplication(0.0, 5.0);
-        assertEquals(148.4131591, newLog.invNatLog((Math.log(0.0)), 5.0), 0.0000001);
+        MainApplication newLog = new MainApplication(5.0);
+        assertEquals(148.4131591, newLog.invNatLog(5.0), 0.0000001);
     }
 
 }
