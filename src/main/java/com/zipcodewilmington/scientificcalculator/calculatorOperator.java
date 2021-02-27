9698e26fc2c1e2aa74912ca2b626571ac6c565b0
r-m-hufford <ryan@rmhufford.com>
2021-02-27 07:58:35 -0500

    added power method

--- src/main/java/com/zipcodewilmington/scientificcalculator/calculatorOperator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/calculatorOperator.java
@@ -1,4 +1,9 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class calculatorOperator {
+
+    public int power (int a, int b) {
+        int power = (int) Math.pow(a,b);
+        return power;
+    }
 }
