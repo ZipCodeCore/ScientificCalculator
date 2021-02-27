cb3fbe771b863f48be17b5b4ce55aa4347ac59e3
Chris Allen <Chris@zipcodes-MacBook-Pro-350.local>
2021-02-26 20:11:34 -0500

    Updated threshholdReached() and created test1()

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -4,73 +4,73 @@ public class Calculator {
     public Calculator() {
     }
 
-    public double sine(int x){
+    public static double sine(int x){
         double ans = Math.sin(x);
         return ans;
     }
-    public double sine(double x){
+    public static double sine(double x){
         double ans = Math.sin(x);
         return ans;
     }
-    public double cosine(int x){
+    public static double cosine(int x){
         double ans = Math.cos(x);
         return ans;
     }
-    public double cosine(double x){
+    public static double cosine(double x){
         double ans = Math.cos(x);
         return ans;
     }
-    public double tangent(int x){
+    public static double tangent(int x){
         double ans = Math.tan(x);
         return ans;
     }
-    public double tangent(double x){
+    public static double tangent(double x){
         double ans = Math.tan(x);
         return ans;
     }
-    public double inverseSine(int x){
+    public static double inverseSine(int x){
         double ans = Math.asin(x);
         return ans;
     }
-    public double inverseSine(double x){
+    public static double inverseSine(double x){
         double ans = Math.asin(x);
         return ans;
     }
-    public double inverseCosine(int x){
+    public static double inverseCosine(int x){
         double ans = Math.acos(x);
         return ans;
     }
-    public double inverseCosine(double x){
+    public static double inverseCosine(double x){
         double ans = Math.acos(x);
         return ans;
     }
-    public double inverseTangent(int x){
+    public static double inverseTangent(int x){
         double ans = Math.atan(x);
         return ans;
     }
-    public double inverseTangent(double x){
+    public static double inverseTangent(double x){
         double ans = Math.atan(x);
         return ans;
     }
 
-    public double toRadians(int x){
+    public static double toRadians(int x){
         double ans = Math.toRadians(x);
         return ans;
     }
-    public double toRadians(double x){
+    public static double toRadians(double x){
         double ans = Math.toRadians(x);
         return ans;
     }
-    public double toDegrees(int x){
+    public static double toDegrees(int x){
         double ans = Math.toDegrees(x);
         return ans;
     }
-    public double toDegrees(double x){
+    public static double toDegrees(double x){
         double ans = Math.toDegrees(x);
         return ans;
     }
 
-    public int factorial(int x){
+    public static int factorial(int x){
         int ans = 1;
         for(int i=1;i<=x;i++){
             ans*=i;
--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -11,12 +11,15 @@ public class MainApplication {
         Integer i = Console.getIntegerInput("Please enter your first number or enter \"Memory\" for your stored value");
 
         Console.println("Please enter the operation you would like to perform:");
-        Console.println("Current Number:                           %s\n",i);
-        Console.println("Add      Subtract      Multiply      Divide ");
-        Console.println("Square   Square Root   Exponential   Inverse");
-        Console.println("Sine     Cosine        Tangent              ");
+        Console.println("Current Number:                              %s\n",i);
+        Console.println("Add      Subtract      Multiply      Divide   ");
+        Console.println("Square   Square Root   Exponential   Inverse  ");
+        Console.println("Log      Log-1         Ln            Ln-1     ");
+        Console.println("Sine     Cosine        Tangent       Factorial");
         Console.println("Sine-1   Cosine-1      Tangent-1              ");
         String j = Console.getStringInput();
+
+
 
         //Console.println("The user input %s as a string", s);
         //Console.println("The user input %s as a d", d);
