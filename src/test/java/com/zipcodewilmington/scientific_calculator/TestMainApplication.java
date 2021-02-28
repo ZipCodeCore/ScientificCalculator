c873109cb5c7c6efd28bfa293dc0fbbbbdd2512d
Jorge <jorge@zipcodes-MacBook-Pro-51.local>
2021-02-28 09:20:02 -0500

    cleaned up testClass

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -59,35 +59,6 @@ public class TestMainApplication {
     }
 
 
-    @Test //Log base 10 method test
-    public void getLog10() {
-        MainApplication newLog = new MainApplication();
-        assertEquals(1.0, newLog.logarithm(10.0), 0.0000001);
-    }
-    @Test //Log base 10 method Err test
-    public void getLog101() {
-        MainApplication newLog = new MainApplication();
-        assertEquals(Double. NEGATIVE_INFINITY, newLog.logarithm(0.0), 0.0000001);
-    }
-
-
-    @Test //Inverted log test
-    public void getInvLog10() {
-        MainApplication newLog = new MainApplication();
-        assertEquals(100000.0, newLog.invLog(5.0), 0.0000001);
-    }
-    @Test //Natural Log test
-    public void getNetLog() {
-        MainApplication newLog = new MainApplication();
-        assertEquals(1.609437912, newLog.natLog(5.0), 0.0000001);
-    }
-    @Test //Inverted Natural Log test
-    public void getInvNetLog() {
-        MainApplication newLog = new MainApplication();
-        assertEquals(148.4131591, newLog.invNatLog(5.0), 0.0000001);
-    }
-
-
 
     @Test
     public void displayTest() {
