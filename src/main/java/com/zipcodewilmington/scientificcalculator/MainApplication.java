f74aad35376c5745c81f8e70df2be25776109d85
jorget93 <78943159+jorget93@users.noreply.github.com>
2021-02-27 19:08:22 -0500

    Jorge branch (#2)
    Added/tested logarithm methods
    
    Co-authored-by: Jorge <jorge@zipcodes-MacBook-Pro-51.local>
    Co-authored-by: xyuan04 <78838190+xyuan04@users.noreply.github.com>

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,11 +1,11 @@
 package com.zipcodewilmington.scientificcalculator;
-
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
     private String currentMode = "binary";
     private String currentUnit = "degrees";
+
 
     public static void main(String[] args) {
         Console.println("Welcome to my calculator!");
@@ -18,8 +18,31 @@ public class MainApplication {
         Console.println("The user input %s as a d", d);
 
 
-
     }
+
+    //Takes a value for num2 for the log equation. num1 is not used in this case
+    public static double logarithm(double num1) {
+        double newNum = Math.log10(num1);
+        System.out.println(newNum);
+        return newNum;
+    }
+    public double invLog(double num1) {
+        double newNum = Math.pow(10,num1);
+        System.out.println(newNum);
+        return newNum;
+    }
+    public double natLog(double num1) {
+        double newNum = Math.log(num1);
+        System.out.println(newNum);
+        return newNum;
+    }
+    public double invNatLog(double num1) {
+        double newNum = Math.pow(Math.E,num1);
+        System.out.println(newNum);
+        return newNum;
+    }
+
+
     //DISPLAY CHANGES
 
     public void getMode() {
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,14 +1,47 @@
 package com.zipcodewilmington.scientific_calculator;
 import static org.junit.Assert.assertEquals;
-
 import com.zipcodewilmington.scientificcalculator.MainApplication;
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 /**
  * Created by leon on 2/9/18.
  */
+
 public class TestMainApplication {
+
+
+    @Test //Log base 10 method test
+    public void getLog10() {
+        MainApplication newLog = new MainApplication();
+        assertEquals(1.0, newLog.logarithm(10.0), 0.0000001);
+    }
+    @Test //Log base 10 method Err test
+    public void getLog101() {
+        MainApplication newLog = new MainApplication();
+        assertEquals(Double. NEGATIVE_INFINITY, newLog.logarithm(0.0), 0.0000001);
+    }
+
+
+    @Test //Inverted log test
+    public void getInvLog10() {
+        MainApplication newLog = new MainApplication();
+        assertEquals(100000.0, newLog.invLog(5.0), 0.0000001);
+    }
+    @Test //Natural Log test
+    public void getNetLog() {
+        MainApplication newLog = new MainApplication();
+        assertEquals(1.609437912, newLog.natLog(5.0), 0.0000001);
+    }
+    @Test //Inverted Natural Log test
+    public void getInvNetLog() {
+        MainApplication newLog = new MainApplication();
+        assertEquals(148.4131591, newLog.invNatLog(5.0), 0.0000001);
+    }
+
 
     @Test
     public void displayTest() {
@@ -45,6 +78,7 @@ public class TestMainApplication {
         assertEquals("Err", unitChanger1.switchUnitsMode("Break me"));
         assertEquals("degrees", unitChanger1.switchUnitsMode("degrees"));
     }
+
 
 
 
