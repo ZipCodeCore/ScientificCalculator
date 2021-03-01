63fe45bc11c3cfcd69718250353ca8cc770773cf
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-02-28 21:51:20 -0500

    Filled out UML and tests

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -1,29 +1,30 @@
 package com.zipcodewilmington.scientificcalculator;
-
 public class Calculator {
     public Calculator() {
     }
-
     public void drawScreen(String d){
-
         Console.println("");
         Console.println("|------------------------------------------------|");
-        Console.println("|Clear                                    Memory |");
+        Console.println("|Quit     Clear     Set     M+     MC     MRC    |");
         Console.println("|------------------------------------------------|");
-        Console.println("| Current Number:                            %s  |",d);
+        Console.println(" Current Number: %s                               ",d);
         Console.println("|------------------------------------------------|");
         Console.println("| Add      Subtract      Multiply      Divide    |");
         Console.println("| Square   Square Root   Exponential   Inverse   |");
         Console.println("| Log      Log-1         Ln            Ln-1      |");
         Console.println("| Sine     Cosine        Tangent       Factorial |");
         Console.println("| Sine-1   Cosine-1      Tangent-1     Invert    |");
+        Console.println("| cToF     fToC          LBtoKG        KGtoLB    |");
+        Console.println("|          radToDeg      degToRad                |");
         Console.println("|------------------------------------------------|");
     }
     public double performOperation(String op, double num1, double num2){
         double returnAns;
-
         if(op.equalsIgnoreCase("add")){
             returnAns = this.addition(num1,num2);
+
+            double newThing = MainApplication.getCurrent();
+
         }
         else if(op.equalsIgnoreCase("subtract")){
             returnAns = this.subtraction(num1,num2);
@@ -34,19 +35,14 @@ public class Calculator {
         else if(op.equalsIgnoreCase("divide")){
             returnAns = this.divide(num1, num2);
         }
-        else if(op.equalsIgnoreCase("exponential")){
-            returnAns = this.exponential(num1, num2);
-        }
-        //below is placeholder
+        //catching exponential operation
         else {
-            returnAns = 69;
+            returnAns = this.exponential(num1, num2);
         }
         return returnAns;
     }
-
     public double performOperation(String op, double num){
         double returnAns;
-
         if(op.equalsIgnoreCase("square")){
             returnAns = this.square(num);
         }
@@ -92,100 +88,90 @@ public class Calculator {
         else if(op.equalsIgnoreCase("ln-1")){
             returnAns = this.inverseNaturalLog(num);
         }
-        //below is placeholder
+        else if(op.equalsIgnoreCase("lbToKg")){
+            returnAns = this.convertLBtoKG(num);
+        }
+        else if(op.equalsIgnoreCase("kgtoLb")) {
+            returnAns = this.convertKGtoLB(num);
+        }
+        else if(op.equalsIgnoreCase("radtodeg")) {
+            returnAns = this.toDegrees(num);
+        }
+        else if(op.equalsIgnoreCase("degtorad")) {
+            returnAns = this.toRadians(num);
+        }
+        else if(op.equalsIgnoreCase("fToC")){
+            returnAns = this.convertFtoC(num);
+        }
+        //catching convert C to F
         else {
-            returnAns = 69;
+            returnAns = this.convertCtoF(num);
         }
         return returnAns;
     }
-
     //two variable operations
     public double addition(double x, double y){
         double ans = x + y;
         return ans;
     }
-
     public double subtraction(double x,double y){
         double ans = x - y;
         return ans;
     }
-
     public double multiply(double x,double y){
         double ans = x * y;
         return ans;
     }
-
+    //divide by zero handled in MainApplication
     public double divide(double x,double y){
         double ans = x / y;
         return ans;
     }
-
     public double exponential(double x,double y){
         double ans = Math.pow(x,y);
         return ans;
     }
-
     //one variable operations
     public double square(double x){
         double ans = Math.pow(x,2);
         return ans;
     }
-
     public double squareRoot(double x){
         double ans = Math.sqrt(x);
         return ans;
     }
-
     public double inverse(double x){
         double ans = (1 / x);
         return ans;
     }
-
     public double invert(double x){
         double ans = (x * -1);
         return ans;
     }
-
     public double sine(double x){
         double ans = Math.sin(x);
         return ans;
     }
-
     public double cosine(double x){
         double ans = Math.cos(x);
         return ans;
     }
-
     public double tangent(double x){
         double ans = Math.tan(x);
         return ans;
     }
-
     public double inverseSine(double x){
         double ans = Math.asin(x);
         return ans;
     }
-
     public double inverseCosine(double x){
         double ans = Math.acos(x);
         return ans;
     }
-
     public double inverseTangent(double x){
         double ans = Math.atan(x);
         return ans;
     }
-
-    public double toRadians(double x){
-        double ans = Math.toRadians(x);
-        return ans;
-    }
-
-    public double toDegrees(double x){
-        double ans = Math.toDegrees(x);
-        return ans;
-    }
-
     public double factorial(double x){
         double ans = 1;
         for(int i=1;i<=x;i++){
@@ -193,41 +179,44 @@ public class Calculator {
         }
         return ans;
     }
-
     public double log(double x){
         double ans = Math.log10(x);
         return ans;
     }
-
     public double inverseLog(double x){
         double ans = Math.pow(10,x);
         return ans;
     }
-
     public double naturalLog(double x){
         double ans = Math.log(x);
         return ans;
     }
-
     public double inverseNaturalLog(double x){
         double ans = Math.exp(x);
         return ans;
     }
-
-    private String err(){
-        return "Err";
+    public double convertFtoC(double x) {
+        double fToC = (x - 32) / 1.8;
+        return fToC;
     }
-
-    //Conversions
-    public double convertBinary(double x) {
-        double ans = double.toBinaryString(x);
+    public double convertCtoF(double x) {
+        double cToF = (x * 1.8) + 32;
+        return cToF;
+    }
+    public double convertLBtoKG(double x) {
+        double lbToKg = (x * 0.45359237);
+        return lbToKg;
+    }
+    public double convertKGtoLB(double x){
+        double kgtoLb = (x * 2.20462262);
+        return kgtoLb ;
+    }
+    public double toRadians(double x){
+        double ans = Math.toRadians(x);
         return ans;
     }
-
-    public double convertHex(double x) {
-        double ans = double.toHexString(x);
+    public double toDegrees(double x){
+        double ans = Math.toDegrees(x);
         return ans;
     }
-
-
 }
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -177,15 +177,147 @@ public class TestMainApplication {
         assertEquals(Double.valueOf(2.0), myCalc.inverseNaturalLog(0.69315), 0.0001);
     }
 
-    //@org.junit.Test
-    //public void initial6() {
-    //    Calculator myCalc = new Calculator();
-    //    assertEquals(Double.valueOf(4.0), myCalc.addition(2,2), 0.00001);
-    //}
+    @org.junit.Test
+    public void testConvertFtoC() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(32.22222), myCalc.convertFtoC(90), 0.0001);
+        assertEquals(Double.valueOf(5), myCalc.convertFtoC(41), 0.0001);
+        assertEquals(Double.valueOf(-17.77778), myCalc.convertFtoC(0), 0.0001);
+    }
 
     @org.junit.Test
-    public void testConvertBinary() {
+    public void testConvertCtoF() {
         Calculator myCalc = new Calculator();
-        assertEquals(Double.valueOf(10), myCalc.convertBinary(2));
+        assertEquals(Double.valueOf(90), myCalc.convertCtoF(32.22222), 0.0001);
+        assertEquals(Double.valueOf(41), myCalc.convertCtoF(5), 0.0001);
+        assertEquals(Double.valueOf(0), myCalc.convertCtoF(-17.77778), 0.0001);
     }
+
+    @org.junit.Test
+    public void testConvertLBtoKG() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(0.45359), myCalc.convertLBtoKG(1), 0.0001);
+        assertEquals(Double.valueOf(10.43262), myCalc.convertLBtoKG(23), 0.0001);
+        assertEquals(Double.valueOf(206.8381), myCalc.convertLBtoKG(456), 0.0001);
+    }
+
+    @org.junit.Test
+    public void testConvertKGtoLB() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(1), myCalc.convertKGtoLB(0.45359), 0.0001);
+        assertEquals(Double.valueOf(23), myCalc.convertKGtoLB(10.43262), 0.0001);
+        assertEquals(Double.valueOf(456), myCalc.convertKGtoLB(206.8381), 0.0001);
+    }
+
+    @org.junit.Test
+    public void testToRadians() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(0), myCalc.toRadians(0), 0.0001);
+        assertEquals(Double.valueOf(1.5708), myCalc.toRadians(90), 0.0001);
+        assertEquals(Double.valueOf(3.14159), myCalc.toRadians(180), 0.0001);
+    }
+
+    @org.junit.Test
+    public void testToDegrees() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(0), myCalc.toDegrees(0), 0.001);
+        assertEquals(Double.valueOf(90), myCalc.toDegrees(1.5708), 0.001);
+        assertEquals(Double.valueOf(180), myCalc.toDegrees(3.14159), 0.001);
+    }
+
+    /*@org.junit.Test
+    public void testDecToBinary() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(10), myCalc.decToBinary(2));
+        assertEquals(Double.valueOf(1), myCalc.decToBinary(1));
+        assertEquals(Double.valueOf(1000), myCalc.decToBinary(8));
+    }
+
+    @org.junit.Test
+    public void testDecToOct() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(2), myCalc.decToOct(2));
+        assertEquals(Double.valueOf(10), myCalc.decToOct(8));
+        assertEquals(Double.valueOf(170), myCalc.decToOct(120));
+    }
+
+    @org.junit.Test
+    public void testDecToHex() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(2), myCalc.decToHex(2));
+        assertEquals(Double.valueOf(14), myCalc.decToHex(20));
+        assertEquals("1f", myCalc.decToHex(31));
+    }
+
+    @org.junit.Test
+    public void testBinaryToDec() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(2), myCalc.binaryToDec(10));
+        assertEquals(Double.valueOf(1), myCalc.binaryToDec(1));
+        assertEquals(Double.valueOf(8), myCalc.binaryToDec(1000));
+    }
+
+    @org.junit.Test
+    public void testBinaryToOct() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(2), myCalc.binaryToOct(10));
+        assertEquals(Double.valueOf(17), myCalc.binaryToOct(1111));
+        assertEquals(Double.valueOf(10), myCalc.binaryToOct(1000));
+    }
+
+    @org.junit.Test
+    public void testBinaryToHex() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(2), myCalc.binaryToHex(10));
+        assertEquals(Double.valueOf(10), myCalc.binaryToHex(10000));
+        assertEquals("1f", myCalc.binaryToHex(11111));
+    }
+
+    @org.junit.Test
+    public void testOctToBinary() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(10), myCalc.octToBinary(2));
+        assertEquals(Double.valueOf(1), myCalc.octToBinary(1));
+        assertEquals(Double.valueOf(1000), myCalc.octToBinary(10));
+    }
+
+    @org.junit.Test
+    public void testOctToDec() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(2), myCalc.octToDec(2));
+        assertEquals(Double.valueOf(8), myCalc.octToDec(10));
+        assertEquals(Double.valueOf(120), myCalc.octToDec(170));
+    }
+
+    @org.junit.Test
+    public void testOctToHex() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(2), myCalc.octToHex(2));
+        assertEquals("e", myCalc.octToHex(16));
+        assertEquals(Double.valueOf(19), myCalc.octToHex(31));
+    }
+
+    @org.junit.Test
+    public void testHexToBinary() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(2), myCalc.hexToBinary(2));
+        assertEquals(Double.valueOf(15), myCalc.hexToBinary("f"));
+        assertEquals(Double.valueOf(171), myCalc.hexToBinary("ab"));
+    }
+
+    @org.junit.Test
+    public void testHexToOct() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(2), myCalc.hexToOct(2));
+        assertEquals(Double.valueOf(17), myCalc.hexToOct("f"));
+        assertEquals(Double.valueOf(252), myCalc.hexToOct("ab"));
+    }
+
+    @org.junit.Test
+    public void testHexToDec() {
+        Calculator myCalc = new Calculator();
+        assertEquals(Double.valueOf(10), myCalc.hexToDec(2));
+        assertEquals(Double.valueOf(1111), myCalc.hexToDec("f"));
+        assertEquals(Double.valueOf(10101011), myCalc.hexToDec("ab"));
+    }*/
 }
