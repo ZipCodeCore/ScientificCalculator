d6a3a52bc77db413f61f533c86a6dcb93570925d
Keerthana Srinivasan <keerthihamsa.06@gmail.com>
2021-10-30 17:09:21 -0400

    Revert "changes made in main application"
    This reverts commit 0a5f6c0e0432b6d18ce79e2501f300307f55ca96.

--- src/main/java/com/zipcodewilmington/scientificcalculator/BasicCalculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/BasicCalculator.java
@@ -2,58 +2,58 @@ package com.zipcodewilmington.scientificcalculator;
 
 public class BasicCalculator {
 
-    private double result;
+    private Double result;
 
     public BasicCalculator(){
         result = 0.0;
     }
 
-    public double add(double num1, double num2){
+    public Double add(double num1, double num2){
         result = num1 + num2;
         return result;
     }
 
-    public double subtract(double num1, double num2){
+    public Double subtract(double num1, double num2){
         result = num1 - num2;
         return result;
     }
 
-    public double multiply(double num1, double num2){
+    public Double multiply(double num1, double num2){
         result = num1 * num2;
         return result;
     }
 
-    public double divide(double num1, double num2){
+    public Double divide(double num1, double num2){
         result = num1 / num2;
         return result;
     }
 
-    public double squareRoot(double num1) {
+    public Double squareRoot(Double num1) {
         result = Math.sqrt(num1);
         return result;
     }
 
-    public double square(double num1) {
+    public Double square(Double num1) {
         result = Math.pow(num1, 2);
         return result;
     }
 
-    public double exponentiation(double num1, double num2) {
+    public Double exponentiation(Double num1, Double num2) {
         result = Math.pow(num1, num2);
         return result;
     }
 
-    public double inverse(double num1) {
+    public Double inverse(Double num1) {
         result = 1 / num1;
         return result;
     }
 
-    public double invertNumber(double num1) {
+    public Double invertNumber(Double num1) {
         result = -1 * num1;
         return result;
     }
 
-    public double percentage(double num1) {
+    public Double percentage(Double num1) {
         result = num1 / 100;
         return result;
     }
--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -33,73 +33,68 @@ public class MainApplication {
         // Creating infinite loop until user choose to exit
         while (true) {
 
-            try {
-                if (calculatorType == 1) {
+            if (calculatorType == 1) {
 
-                    int choice = Console.getIntegerInput("Please select from the options below\n " +
-                            "1) Add \n " +
-                            "2) Subtract\n " +
-                            "3) Multiply \n " +
-                            "4) Divison\n " +
-                            "5) Invert\n" +
-                            "6) Percentage\n" +
-                            "7) Switch to Scientific Calculator\n" +
-                            "8) Clear\n" +
-                            "9) Exit", 1, 9);
+                int choice = Console.getIntegerInput("Please select from the options below\n " +
+                        "1) Add \n " +
+                        "2) Subtract\n " +
+                        "3) Multiply \n " +
+                        "4) Divison\n " +
+                        "5) Invert\n" +
+                        "6) Percentage\n" +
+                        "7) Switch to Scientific Calculator\n" +
+                        "8) Clear\n" +
+                        "9) Exit", 1, 9);
 
-                    if (choice >= 1 && choice <= 4) {
-                        operandValue = Console.getDoubleInput("Please enter the operand number");
-                    }
+                if (choice >= 1 && choice <= 4) {
+                    operandValue = Console.getDoubleInput("Please enter the operand number");
+                }
 
-                    switch (choice) {
-                        case 1:
-                            currentValue = basicCalc.add(currentValue, operandValue);
-                            Console.displayValue(currentValue);
-                            break;
-                        case 2:
-                            currentValue = basicCalc.subtract(currentValue, operandValue);
-                            Console.displayValue(currentValue);
-                            break;
-                        case 3:
-                            currentValue = basicCalc.multiply(currentValue, operandValue);
-                            Console.displayValue(currentValue);
-                            break;
-                        case 4:
-                            currentValue = basicCalc.divide(currentValue, operandValue);
-                            if (Double.isInfinite(currentValue)) {
-                                Console.println(">>>>>>>>   Display Value: %s <<<<<<<<<< ", "Err");
-                            }
-                            currentValue = Console.getDoubleInput("Please enter the number");
-                            Console.displayValue(currentValue);
-                            break;
-                        case 5:
-                            currentValue = basicCalc.invertNumber(currentValue);
-                            Console.displayValue(currentValue);
-                            break;
-                        case 6:
-                            currentValue = basicCalc.percentage(currentValue);
-                            Console.displayValue(currentValue);
-                            break;
-                        case 7:
-                            calculatorType = 2;
-                            Console.println("Switching to Scientific Calculator");
-                            Console.displayValue(currentValue);
-                            break;
-                        case 8:
-                            currentValue = 0.0;
-                            Console.displayValue(currentValue);
-                            currentValue = Console.getDoubleInput("Please enter the number");
-                            Console.displayValue(currentValue);
-                            break;
-                    }
-
-                    // Break the while loop when user choose to exit
-                    if (choice == 9) {
+                switch (choice) {
+                    case 1:
+                        currentValue = basicCalc.add(currentValue, operandValue);
+                        Console.displayValue(currentValue);
                         break;
-                    }
-                } else {
+                    case 2:
+                        currentValue = basicCalc.subtract(currentValue, operandValue);
+                        Console.displayValue(currentValue);
+                        break;
+                    case 3:
+                        currentValue = basicCalc.multiply(currentValue, operandValue);
+                        Console.displayValue(currentValue);
+                        break;
+                    case 4:
+                        currentValue = basicCalc.divide(currentValue, operandValue);
+                        Console.displayValue(currentValue);
+                        break;
+                    case 5:
+                        currentValue = basicCalc.invertNumber(currentValue);
+                        Console.displayValue(currentValue);
+                        break;
+                    case 6:
+                        currentValue = basicCalc.percentage(currentValue);
+                        Console.displayValue(currentValue);
+                        break;
+                    case 7:
+                        calculatorType = 2;
+                        Console.println("Switching to Scientific Calculator");
+                        Console.displayValue(currentValue);
+                        break;
+                    case 8:
+                        currentValue = 0.0;
+                        Console.displayValue(currentValue);
+                        currentValue = Console.getDoubleInput("Please enter the number");
+                        Console.displayValue(currentValue);
+                        break;
+                }
 
-                    int choice = Console.getIntegerInput("Please select from the options below\n " +
+                if (choice == 9) {
+                    break;
+                }
+            }
+            else {
+
+                int choice = Console.getIntegerInput("Please select from the options below\n " +
                             "1) degree\n" +
                             "2) natural log\n" +
                             "3) base log\n" +
@@ -117,60 +112,54 @@ public class MainApplication {
                             "15) inverseSine\n" +
                             "16) radian\n" +
                             "17) factorial\n" +
-                            "18) fibonacci\n" +
+                            "18) fibonacci\n"+
                             "19) Switch to Scientific Calculator\n" +
                             "20) Clear\n" +
                             "21) Exit", 1, 21);
-                    switch (choice) {
-                        case 1:
-
-                        case 2:
-
-                            break;
-                        case 3:
-                            //
-                            break;
-                        case 4:
-                            //
-                            break;
-                        case 5:
-                            currentValue = basicCalc.squareRoot(currentValue);
-                            Console.displayValue(currentValue);
-                            break;
-                        case 6:
-                            currentValue = basicCalc.inverse(currentValue);
-                            Console.displayValue(currentValue);
-                            break;
-                        case 7:
-                            currentValue = basicCalc.square(currentValue);
-                            Console.displayValue(currentValue);
-                            break;
-                        case 18:
-                            fibo.fib(currentValue);
-                            currentValue = Console.getDoubleInput("\nPlease enter the number");
-                            Console.displayValue(currentValue);
-                            break;
-                        case 19:
-                            calculatorType = 1;
-                            break;
-                        case 20:
-                            currentValue = 0.0;
-                            Console.displayValue(currentValue);
-                            currentValue = Console.getDoubleInput("Please enter the number");
-                            Console.displayValue(currentValue);
-                            break;
-                    }
-
-                    if (choice == 21) {
+                switch (choice) {
+                    case 1:
+                        //
                         break;
-                    }
+                    case 2:
+                        //
+                        break;
+                    case 3:
+                        //
+                        break;
+                    case 4:
+                        //
+                        break;
+                    case 5:
+                        currentValue = basicCalc.squareRoot(currentValue);
+                        Console.displayValue(currentValue);
+                        break;
+                    case 6:
+                        currentValue = basicCalc.inverse(currentValue);
+                        Console.displayValue(currentValue);
+                        break;
+                    case 7:
+                        currentValue = basicCalc.square(currentValue);
+                        Console.displayValue(currentValue);
+                        break;
+                    case 18:
+                        fibo.fib(currentValue);
+                        currentValue = Console.getDoubleInput("Please enter the number");
+                        Console.displayValue(currentValue);
+                        break;
+                    case 19:
+                        calculatorType = 1;
+                        break;
+                    case 20:
+                        currentValue = 0.0;
+                        Console.displayValue(currentValue);
+                        currentValue = Console.getDoubleInput("Please enter the number");
+                        Console.displayValue(currentValue);
+                        break;
                 }
-            }
-            catch(Exception ex) {
-                // Display error whenever this catch block is called & start again
-                Console.println(">>>>>>>>   Display Value: %s <<<<<<<<<< ", "Err");
-                currentValue = Console.getDoubleInput("Please enter the number");
-                Console.displayValue(currentValue);
+
+                if (choice == 21) {
+                    break;
+                }
             }
         }
 
