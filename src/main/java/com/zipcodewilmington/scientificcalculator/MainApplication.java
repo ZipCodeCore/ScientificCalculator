7d130fa29208c8090b90a44bce6cd37574c41760
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2021-11-01 00:06:04 -0400

    updated

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -44,11 +44,11 @@ public class MainApplication {
     public static String modeSelection(Long standard){
        int modified = standard.intValue();
 
-        if(scientificFunction.switchDisplayMode.equalsIgnoreCase("binary")){
+        if(scientificFunctions.switchDisplayMode().equalsIgnoreCase("binary")){
             return Integer.toBinaryString(modified);
-        }else if (scientificFunction.switchDisplayMode.equalsIgnoreCase("octal")){
+        }else if (scientificFunctions.switchDisplayMode().equalsIgnoreCase("octal")){
             return Integer.toOctalString(modified);
-        }else if (scientificFunction.switchDisplayMode.equalsIgnoreCase("hexadecimal")){
+        }else if (scientificFunctions.switchDisplayMode().equalsIgnoreCase("hexadecimal")){
             return Integer.toHexString(modified);
         }else{
             return Long.toString(standard);
