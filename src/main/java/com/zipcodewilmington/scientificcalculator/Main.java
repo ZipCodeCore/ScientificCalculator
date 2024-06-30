7869102a0d1a2c6e4bfd6e33dcd8146fef855cbb
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-06-29 20:17:28 -0400

    display value patch

--- src/main/java/com/zipcodewilmington/scientificcalculator/Main.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Main.java
@@ -8,29 +8,20 @@ import static com.zipcodewilmington.scientificcalculator.ScannerCalculator.*;
  */
 public class Main{
     public static void main(String[] args) {
-        /*Console.println("Welcome to grop b calculator version 1.0!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
-
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
-        //Features Start = new Features();
-        //System.out.println(Start.theAnswer(7,"*",4)); */
-
 
         Scanner scanner = new Scanner(System.in);
 
         //while loop to keep the program running until the user exits
         while (true) {
             //list of options for the user to choose from
+            System.out.println("Group Orile Calculator Version 1.0");
             System.out.println("Curret value: " + displayValue);
             System.out.println("Choose operation");
             System.out.println("1- Get current value");
             System.out.println("2- Clear display");
             System.out.println("3- Change display value");
             System.out.println("4- Add");
+            System.out.println("0- Exit");
 
             // a case statement to handle the user's choice as an integer
             int choice = scanner.nextInt();
@@ -43,9 +34,19 @@ public class Main{
                     break;
                 case 3:
                     System.out.println("Enter new value: ");
+                    double newValue = scanner.nextDouble();
+                    changeDisplayValue(newValue);
+                    break;
                 case 4:
                     double addValue = scanner.nextDouble();
                     add(addValue);
+                    break;
+                case 0:
+                    System.out.println("Exiting the calculator");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid please try again");
             }
         }
 
--- src/main/java/com/zipcodewilmington/scientificcalculator/ScannerCalculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ScannerCalculator.java
@@ -1,6 +1,10 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.Scanner;
+
 public class ScannerCalculator {
+    static Scanner scanner = new Scanner(System.in);
+
     public static double displayValue = 0.0;
     public static double memoryValue = 0.0;
     public static final String ERROR_MESSAGE = "Err";
@@ -15,13 +19,18 @@ public class ScannerCalculator {
         displayValue = 0.0;
         System.out.println("Display cleared");
     }
-    // changes the value of the display
     public static void changeDisplayValue(double newValue) {
         displayValue = newValue;
+        System.out.println("Display value changed to: " + displayValue);
     }
 
     // addition method
     public static void add(double value) {
         displayValue += value;
+    }
+
+    // subtraction method
+    public static void subtract(double value) {
+        displayValue -= value;
     }
 }
