0cfaf5652050c74cd33e53160e47895b16c30729
igordon215 <igordon215@gmail.com>
2024-06-30 09:33:56 -0400

    changes to main

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -4,6 +4,19 @@ package com.zipcodewilmington.scientificcalculator;
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
+    public static boolean startLoop = true; // TURE OR FALSE
+    private static String currentMode = "binary";//Binary Mode
+    private static String currentUnit = "degrees";//Degrees Mode
+    private static double currentValue = 0.0; // List Current Value
+    private static double memoryValue = 0.0;  // List  Memory Value
+    private static String displayValue = ""; //String.valueOf(currentValue)
+
+
+
+
+
+
+
     public static void main(String[] args) {
         Console.println("Welcome to my calculator!");
         String s = Console.getStringInput("Enter a string");
